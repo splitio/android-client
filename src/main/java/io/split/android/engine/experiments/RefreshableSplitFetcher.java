bf13ef4a51a0cd9682919bb7459ee385c33a1c95
Fernando Martin <fernando@split.io>
2018-07-19 13:52:41 -0700

    revert

--- src/main/java/io/split/android/engine/experiments/RefreshableSplitFetcher.java
+++ src/main/java/io/split/android/engine/experiments/RefreshableSplitFetcher.java
@@ -71,18 +71,15 @@ public class RefreshableSplitFetcher implements SplitFetcher, Runnable {
 
         Map<String, ParsedSplit> toAdd = Maps.newHashMap();
 
-        if (change.splits != null && !change.splits.isEmpty()) {
-            for (Split split : change.splits) {
-                if (split.status != null && split.name != null) {
-                    if (Status.ACTIVE.equals(split.status)) {
-                        ParsedSplit parsedSplit = _parser.parse(split);
-                        if (parsedSplit == null) {
-                            Logger.i("We could not parse the experiment definition for: %s so we are removing it completely to be careful", split.name);
-                            continue;
-                        }
-                        toAdd.put(split.name, parsedSplit);
-                    }
+        for (Split split : change.splits) {
+
+            if (split.status == Status.ACTIVE) {
+                ParsedSplit parsedSplit = _parser.parse(split);
+                if (parsedSplit == null) {
+                    Logger.i("We could not parse the experiment definition for: %s so we are removing it completely to be careful", split.name);
+                    continue;
                 }
+                toAdd.put(split.name, parsedSplit);
             }
         }
 
