ab85b0c0b7b49247fb433516dd0909115089e83a
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-12-14 09:40:12 -0300

    Store my segments in separate files for each matching key

--- src/main/java/io/split/android/client/cache/MySegmentsCache.java
+++ src/main/java/io/split/android/client/cache/MySegmentsCache.java
@@ -11,6 +11,7 @@ import io.split.android.client.dtos.MySegment;
 import io.split.android.client.storage.IStorage;
 import io.split.android.client.utils.Json;
 import io.split.android.client.utils.Logger;
+import io.split.android.client.utils.Utils;
 
 /**
  * Created by guillermo on 12/28/17.
@@ -26,8 +27,8 @@ public class MySegmentsCache implements IMySegmentsCache {
         _storage = storage;
     }
 
-    private String getMySegmentsId() {
-        return MY_SEGMENTS_FILE_PREFIX;
+    private String getMySegmentsId(String key) {
+        return MY_SEGMENTS_FILE_PREFIX + "_" + Utils.sanitizeForFileName(key);
     }
 
     private String getMySegmentsKeyId() {
@@ -38,7 +39,7 @@ public class MySegmentsCache implements IMySegmentsCache {
     public boolean saveMySegments(String key, List<MySegment> mySegments) {
         try {
             _storage.write(getMySegmentsKeyId(), key);
-            _storage.write(getMySegmentsId(), Json.toJson(mySegments));
+            _storage.write(getMySegmentsId(key), Json.toJson(mySegments));
             return true;
         } catch (IOException e) {
             Logger.e(e, "Could not save my segments");
@@ -51,13 +52,13 @@ public class MySegmentsCache implements IMySegmentsCache {
         try {
             String savedKey = _storage.read(getMySegmentsKeyId());
             if (savedKey != null && savedKey.equals(key)) {
-                String storedMySegments = _storage.read(getMySegmentsId());
+                String storedMySegments = _storage.read(getMySegmentsId(key));
                 Type listType = new TypeToken<List<MySegment>>() {
                 }.getType();
 
                 return Json.fromJson(storedMySegments, listType);
             } else {
-                _storage.delete(getMySegmentsId());
+                _storage.delete(getMySegmentsId(key));
             }
         } catch (IOException e) {
             Logger.e(e, "Unable to get my segments");
@@ -66,7 +67,7 @@ public class MySegmentsCache implements IMySegmentsCache {
     }
 
     @Override
-    public void deleteMySegments() {
-        _storage.delete(getMySegmentsId());
+    public void deleteMySegments(String key) {
+        _storage.delete(getMySegmentsId(key));
     }
 }
