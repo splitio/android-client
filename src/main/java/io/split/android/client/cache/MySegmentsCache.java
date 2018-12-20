bbfd3171132408a8627f19313ba1e76dc2a447ee
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-12-20 10:19:11 -0300

    Added load from disk function to My Segments Cache

--- src/main/java/io/split/android/client/cache/MySegmentsCache.java
+++ src/main/java/io/split/android/client/cache/MySegmentsCache.java
@@ -4,8 +4,14 @@ import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
 import io.split.android.client.api.Key;
 import io.split.android.client.dtos.MySegment;
 import io.split.android.client.storage.IStorage;
@@ -16,14 +22,16 @@ import io.split.android.client.utils.Logger;
  * Created by guillermo on 12/28/17.
  */
 
-public class MySegmentsCache implements IMySegmentsCache {
+public class MySegmentsCache implements IMySegmentsCache, LifecycleObserver {
 
     private static final String MY_SEGMENTS_FILE_PREFIX = "SPLITIO.mysegments";
 
     private final IStorage _storage;
+    private final Set<String> _segments;
 
     public MySegmentsCache(IStorage storage) {
         _storage = storage;
+        _segments = Collections.synchronizedSet(new HashSet<>());
     }
 
     private String getMySegmentsId() {
@@ -69,4 +77,31 @@ public class MySegmentsCache implements IMySegmentsCache {
     public void deleteMySegments() {
         _storage.delete(getMySegmentsId());
     }
+
+
+    public void loadFromFile(String key) {
+        try {
+            String savedKey = _storage.read(getMySegmentsKeyId());
+            if (savedKey != null && savedKey.equals(key)) {
+                String storedMySegments = _storage.read(getMySegmentsId());
+                Type listType = new TypeToken<List<MySegment>>() {
+                }.getType();
+
+                List<String> segments = Json.fromJson(storedMySegments, listType);
+                _segments.addAll(segments);
+
+            } else {
+                _storage.delete(getMySegmentsId());
+            }
+        } catch (IOException e) {
+            Logger.e(e, "Unable to get my segments. It could be no segments for this key on disk");
+        }
+    }
+
+    // Lifecyle observer
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    public void disconnectListener() {
+
+    }
+
 }
