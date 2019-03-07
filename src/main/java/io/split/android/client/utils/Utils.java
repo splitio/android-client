5357565049e1da72fbe746bd41cfe19846d985a4
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-03-07 07:11:28 -0300

    Added function to create folder data name based on ApiKey using BCrypt hashing algorithm

--- src/main/java/io/split/android/client/utils/Utils.java
+++ src/main/java/io/split/android/client/utils/Utils.java
@@ -1,5 +1,7 @@
 package io.split.android.client.utils;
 
+import com.google.common.base.Strings;
+
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
@@ -90,5 +92,31 @@ public class Utils {
             return "";
         }
         return string.replaceAll("[^a-zA-Z0-9\\.\\-]", "_");
+    }
+
+    public static String sanitizeForFolderName(String string) {
+        if(string == null) {
+            return "";
+        }
+        return string.replaceAll("[^a-zA-Z0-9]", "");
+    }
+
+    public static String convertApiKeyToFolder(String apiKey) {
+        final int SALT_LENGTH = 29;
+        final String SALT_PREFIX = "$2a$10$";
+        final String CHAR_TO_FILL_SALT = "A";
+        String sanitizedApiKey = sanitizeForFolderName(apiKey);
+        StringBuilder salt = new StringBuilder(SALT_PREFIX);
+        if (sanitizedApiKey.length() >= SALT_LENGTH - SALT_PREFIX.length()) {
+            salt.append(sanitizedApiKey.substring(0, SALT_LENGTH - SALT_PREFIX.length()));
+        } else {
+            salt.append(sanitizedApiKey);
+            salt.append(Strings.repeat(CHAR_TO_FILL_SALT, (SALT_LENGTH - SALT_PREFIX.length()) - sanitizedApiKey.length()));
+        }
+        // Remove last end of strings
+        String cleanedSalt = salt.toString().substring(0, 29);
+        String hash = BCrypt.hashpw(sanitizedApiKey, cleanedSalt);
+
+        return (hash != null ? sanitizeForFolderName(hash) : null);
     }
 }
