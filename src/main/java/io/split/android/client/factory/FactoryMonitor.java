cc3b9ff1bcf857a65014b404681686bb25e0225c
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-05-14 11:28:50 -0300

    Added Factory monitor for multi factory validation, tests and validations

--- src/main/java/io/split/android/client/factory/FactoryMonitor.java
+++ src/main/java/io/split/android/client/factory/FactoryMonitor.java
@@ -3,7 +3,8 @@ package io.split.android.client.factory;
 import io.split.android.client.SplitFactory;
 
 public interface FactoryMonitor {
-    int allCount();
-    int instanceCount(String apiKey);
-    void register(SplitFactory instance, String apiKey);
+    int count();
+    int count(String apiKey);
+    void add(String apiKey);
+    void remove(String apiKey);
 }
--- src/main/java/io/split/android/client/factory/FactoryMonitorImpl.java
+++ src/main/java/io/split/android/client/factory/FactoryMonitorImpl.java
@@ -1,23 +1,61 @@
 package io.split.android.client.factory;
 
+import android.support.annotation.VisibleForTesting;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import io.split.android.client.SplitFactory;
 
 public class FactoryMonitorImpl implements FactoryMonitor {
 
-    private FactoryRegistry factoryRegistry;
+    private Map<String, Integer> factories;
 
-    @Override
-    public int allCount() {
-        return factoryRegistry.count();
+    private static FactoryMonitor sharedInstance;
+
+    @VisibleForTesting
+    public FactoryMonitorImpl() {
+        factories = new HashMap<>();
+    }
+
+    public static FactoryMonitor getSharedInstance() {
+        if(sharedInstance == null) {
+            sharedInstance = new FactoryMonitorImpl();
+        }
+        return sharedInstance;
     }
 
     @Override
-    public int instanceCount(String apiKey) {
-        return factoryRegistry.count(apiKey);
+    public synchronized int count() {
+        int totalCount = 0;
+        for(Integer count : factories.values()) {
+            totalCount += count.intValue();
+        }
+        return totalCount;
     }
 
     @Override
-    public void register(SplitFactory instance, String apiKey) {
-        factoryRegistry.append(new WeakFactory(instance), apiKey);
+    public synchronized int count(String apiKey) {
+        Integer count = factories.get(apiKey);
+        return (count != null ? count.intValue() : 0);
+    }
+
+    @Override
+    public synchronized void add(String apiKey) {
+        Integer count = factories.get(apiKey);
+        int newCount = (count != null ? count.intValue() : 0) + 1;
+        factories.put(apiKey, new Integer(newCount));
+    }
+
+    @Override
+    public synchronized void remove(String apiKey) {
+        Integer count = factories.get(apiKey);
+        int newCount = (count != null ? count.intValue() : 0) - 1;
+        if(newCount > 0) {
+            factories.put(apiKey, new Integer(newCount));
+        } else {
+            factories.remove(apiKey);
+        }
     }
 }
