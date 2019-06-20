8c4419fd6fc1fa316a77aaf97fbfd5af0c2c03a7
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-06-20 11:18:14 -0300

    Added some http client tests

--- src/test/java/io/split/android/HttpClientTest.java
+++ src/test/java/io/split/android/HttpClientTest.java
@@ -1,5 +1,7 @@
 package io.split.android;
 
+import com.google.gson.reflect.TypeToken;
+
 import junit.framework.Assert;
 
 import org.apache.http.client.utils.URIBuilder;
@@ -7,13 +9,20 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import io.split.android.client.dtos.MySegment;
+import io.split.android.client.dtos.SplitChange;
 import io.split.android.client.network.HttpClient;
 import io.split.android.client.network.HttpClientImpl;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
 
 import io.split.android.client.network.HttpRequest;
 import io.split.android.client.network.HttpResponse;
+import io.split.android.client.utils.Json;
+import io.split.android.helpers.FileHelper;
 import okhttp3.HttpUrl;
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
@@ -33,11 +42,41 @@ public class HttpClientTest {
         @Test
         public void severalRequest() throws Exception {
             HttpClient client  = new HttpClientImpl();
-            HttpUrl u = mWebServer.url("/test1/");
-            HttpRequest request = client.request(u.uri(), HttpClient.HTTP_GET);
-            HttpResponse response = request.execute();
-            Assert.assertEquals(200, response.getHttpStatus());
-            Assert.assertEquals("this is split test", response.getData());
+
+            // Test dummy request and response
+            HttpUrl url = mWebServer.url("/test1/");
+            HttpRequest dummyReq = client.request(url.uri(), HttpClient.HTTP_GET);
+            HttpResponse dummyResp = dummyReq.execute();
+
+            // Test my segments
+            url = mWebServer.url("/test2/");
+            HttpRequest mySegmentsReq = client.request(url.uri(), HttpClient.HTTP_GET);
+            HttpResponse mySegmentsResp = mySegmentsReq.execute();
+
+            // Test split changes
+            url = mWebServer.url("/test3/");
+            HttpRequest splitChangeReq = client.request(url.uri(), HttpClient.HTTP_GET);
+            HttpResponse splitChangeResp = splitChangeReq.execute();
+
+            // Assert dummy request and response
+            Assert.assertEquals(200, dummyResp.getHttpStatus());
+            Assert.assertEquals("this is split test", dummyResp.getData());
+
+            // Assert my segments
+            List<MySegment> mySegments = parseMySegments(mySegmentsResp.getData());
+            Assert.assertEquals(200, mySegmentsResp.getHttpStatus());
+            Assert.assertEquals(2, mySegments.size());
+            Assert.assertEquals("id1", mySegments.get(0).id);
+            Assert.assertEquals("groupa", mySegments.get(0).name);
+            Assert.assertEquals("id2", mySegments.get(1).id);
+            Assert.assertEquals("groupb", mySegments.get(1).name);
+
+            // Assert split changes
+            SplitChange splitChange = Json.fromJson(splitChangeResp.getData(), SplitChange.class);
+            Assert.assertEquals(200, splitChangeResp.getHttpStatus());
+            Assert.assertEquals(-1, splitChange.since);
+            Assert.assertEquals(1506703262916L, splitChange.till);
+            Assert.assertEquals(30, splitChange.splits.size());
         }
 
         @After
@@ -46,6 +85,9 @@ public class HttpClientTest {
         }
 
         private void setupServer() throws IOException {
+
+            final String splitChangesResponse = new FileHelper().loadFileContent("split_changes_1.json");
+
             mWebServer = new MockWebServer();
             final Dispatcher dispatcher = new Dispatcher() {
 
@@ -56,9 +98,9 @@ public class HttpClientTest {
                         case "/test1/":
                             return new MockResponse().setResponseCode(200).setBody("this is split test");
                         case "/test2/":
-                            return new MockResponse().setResponseCode(200).setBody("this\nis\nsplit\ntest");
+                            return new MockResponse().setResponseCode(200).setBody("{\"mySegments\":[{\"id\":\"id1\", \"name\":\"groupa\"}, {\"id\":\"id2\", \"name\":\"groupb\"}]}");
                         case "/test3/":
-                            return new MockResponse().setResponseCode(200).setBody("{\\\"info\\\":{\\\"name\":\"Lucas Albuquerque\",\"age\":\"21\",\"gender\":\"male\"}}");
+                            return new MockResponse().setResponseCode(200).setBody(splitChangesResponse);
                     }
                     return new MockResponse().setResponseCode(404);
                 }
@@ -69,6 +111,14 @@ public class HttpClientTest {
 
         private void log(String m) {
             System.out.println("FACTORY_TEST: " + m);
+        }
+
+        private List<MySegment> parseMySegments(String json) {
+            Type mapType = new TypeToken<Map<String, List<MySegment>>>() {
+            }.getType();
+
+            Map<String, List<MySegment>> mySegmentsMap = Json.fromJson(json, mapType);
+            return mySegmentsMap.get("mySegments");
         }
 
     }
