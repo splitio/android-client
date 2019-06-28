f09eaded445a63f74c55c496555c614ecb8c0cf1
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-06-28 09:31:05 -0300

    Added impressions post tests for httpclient

--- src/test/java/io/split/android/HttpClientTest.java
+++ src/test/java/io/split/android/HttpClientTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import io.split.android.client.dtos.Event;
 import io.split.android.client.dtos.MySegment;
 import io.split.android.client.dtos.SplitChange;
+import io.split.android.client.dtos.TestImpressions;
 import io.split.android.client.network.HttpClient;
 import io.split.android.client.network.HttpClientImpl;
 
@@ -32,196 +33,218 @@ import okhttp3.mockwebserver.RecordedRequest;
 
 public class HttpClientTest {
 
-        MockWebServer mWebServer;
-        String mTrackRequestBody = null;
+    MockWebServer mWebServer;
+    String mTrackRequestBody = null;
 
-        @Before
-        public void setup() throws IOException {
-            setupServer();
-        }
+    @Before
+    public void setup() throws IOException {
+        setupServer();
+    }
 
-        @Test
-        public void severalRequest() throws Exception {
-            RecordedRequest recReq;
-            HttpClient client  = new HttpClientImpl();
+    @Test
+    public void severalRequest() throws Exception {
+        RecordedRequest recReq;
+        HttpClient client = new HttpClientImpl();
 
-            // Test dummy request and response
-            HttpUrl url = mWebServer.url("/test1/");
-            HttpRequest dummyReq = client.request(url.uri(), HttpClient.HTTP_GET);
-            HttpResponse dummyResp = dummyReq.execute();
-            recReq = mWebServer.takeRequest();
+        // Test dummy request and response
+        HttpUrl url = mWebServer.url("/test1/");
+        HttpRequest dummyReq = client.request(url.uri(), HttpClient.HTTP_GET);
+        HttpResponse dummyResp = dummyReq.execute();
+        recReq = mWebServer.takeRequest();
 
-            // Test my segments
-            url = mWebServer.url("/test2/");
-            HttpRequest mySegmentsReq = client.request(url.uri(), HttpClient.HTTP_GET);
-            HttpResponse mySegmentsResp = mySegmentsReq.execute();
-            recReq = mWebServer.takeRequest();
+        // Test my segments
+        url = mWebServer.url("/test2/");
+        HttpRequest mySegmentsReq = client.request(url.uri(), HttpClient.HTTP_GET);
+        HttpResponse mySegmentsResp = mySegmentsReq.execute();
+        recReq = mWebServer.takeRequest();
 
-            // Test split changes
-            url = mWebServer.url("/test3/");
-            HttpRequest splitChangeReq = client.request(url.uri(), HttpClient.HTTP_GET);
-            HttpResponse splitChangeResp = splitChangeReq.execute();
-            recReq = mWebServer.takeRequest();
+        // Test split changes
+        url = mWebServer.url("/test3/");
+        HttpRequest splitChangeReq = client.request(url.uri(), HttpClient.HTTP_GET);
+        HttpResponse splitChangeResp = splitChangeReq.execute();
+        recReq = mWebServer.takeRequest();
 
-            // Test empty response
-            url = mWebServer.url("/test4/");
-            HttpRequest emptyReq = client.request(url.uri(), HttpClient.HTTP_GET);
-            HttpResponse emptyResp = emptyReq.execute();
-            recReq = mWebServer.takeRequest();
+        // Test empty response
+        url = mWebServer.url("/test4/");
+        HttpRequest emptyReq = client.request(url.uri(), HttpClient.HTTP_GET);
+        HttpResponse emptyResp = emptyReq.execute();
+        recReq = mWebServer.takeRequest();
 
-            // Test post with response data
-            url = mWebServer.url("/post_resp/");
-            HttpRequest postDataReq = client.request(url.uri(), HttpClient.HTTP_POST);
-            HttpResponse postDataResp = postDataReq.execute();
-            recReq = mWebServer.takeRequest();
+        // Test post with response data
+        url = mWebServer.url("/post_resp/");
+        HttpRequest postDataReq = client.request(url.uri(), HttpClient.HTTP_POST);
+        HttpResponse postDataResp = postDataReq.execute();
+        recReq = mWebServer.takeRequest();
 
-            // Test post no response data
-            url = mWebServer.url("/post_no_resp/");
-            HttpRequest postNoDataReq = client.request(url.uri(), HttpClient.HTTP_POST);
-            HttpResponse postNoDataResp = postNoDataReq.execute();
-            recReq = mWebServer.takeRequest();
+        // Test post no response data
+        url = mWebServer.url("/post_no_resp/");
+        HttpRequest postNoDataReq = client.request(url.uri(), HttpClient.HTTP_POST);
+        HttpResponse postNoDataResp = postNoDataReq.execute();
+        recReq = mWebServer.takeRequest();
 
-            // Test post tracks
-            String postTracksData = new FileHelper().loadFileContent("tracks_1.json");
-            url = mWebServer.url("/tracks/");
-            HttpRequest postTracksReq = client.request(url.uri(), HttpClient.HTTP_POST, postTracksData);
-            HttpResponse postTracksResp = postTracksReq.execute();
-            RecordedRequest postTrackRecReq = mWebServer.takeRequest();
-            String receivedPostTrackData = postTrackRecReq.getBody().readUtf8();
-            List<Event> trackEventsSent = parseTrackEvents(receivedPostTrackData);
-
-            // Test limit wrong request
-            url = mWebServer.url("/limit_wrong/");
-            HttpRequest limitNoSuccessReq = client.request(url.uri(), HttpClient.HTTP_GET);
-            HttpResponse limitNoSuccessResp = limitNoSuccessReq.execute();
-            recReq = mWebServer.takeRequest();
-
-            // Test bad request
-            url = mWebServer.url("/bad/");
-            HttpRequest badReq = client.request(url.uri(), HttpClient.HTTP_GET);
-            HttpResponse badResp = badReq.execute();
-            recReq = mWebServer.takeRequest();
-
-            // Assert dummy request and response
-            Assert.assertEquals(200, dummyResp.getHttpStatus());
-            Assert.assertTrue(dummyResp.isSuccess());
-            Assert.assertEquals("this is split test", dummyResp.getData());
-
-            // Assert my segments
-            List<MySegment> mySegments = parseMySegments(mySegmentsResp.getData());
-            Assert.assertEquals(200, mySegmentsResp.getHttpStatus());
-            Assert.assertEquals(2, mySegments.size());
-            Assert.assertEquals("id1", mySegments.get(0).id);
-            Assert.assertEquals("groupa", mySegments.get(0).name);
-            Assert.assertEquals("id2", mySegments.get(1).id);
-            Assert.assertEquals("groupb", mySegments.get(1).name);
-
-            // Assert split changes
-            SplitChange splitChange = Json.fromJson(splitChangeResp.getData(), SplitChange.class);
-            Assert.assertEquals(200, splitChangeResp.getHttpStatus());
-            Assert.assertTrue(splitChangeResp.isSuccess());
-            Assert.assertEquals(-1, splitChange.since);
-            Assert.assertEquals(1506703262916L, splitChange.till);
-            Assert.assertEquals(30, splitChange.splits.size());
-
-            // Assert empty response
-            Assert.assertEquals(200, emptyResp.getHttpStatus());
-            Assert.assertTrue(emptyResp.isSuccess());
-            Assert.assertNull(emptyResp.getData());
-
-            // Assert post non empty response
-            Assert.assertEquals(201, postDataResp.getHttpStatus());
-            Assert.assertTrue(postDataResp.isSuccess());
-            Assert.assertEquals("{\"resp\": 1 }", postDataResp.getData());
-
-            // Assert empty response
-            Assert.assertEquals(201, postNoDataResp.getHttpStatus());
-            Assert.assertTrue(postNoDataResp.isSuccess());
-            Assert.assertNull(postNoDataResp.getData());
-
-            // Assert tracks
-            Assert.assertEquals(200, postTracksResp.getHttpStatus());
-            Assert.assertTrue(postTracksResp.isSuccess());
-            Assert.assertNull(postTracksResp.getData());
-            Assert.assertEquals(10, trackEventsSent.size());
-            Assert.assertEquals("open_web", trackEventsSent.get(0).eventTypeId);
-            Assert.assertEquals("CUSTOMER_ID", trackEventsSent.get(0).key);
-            Assert.assertEquals(1.0, trackEventsSent.get(0).value);
-            Assert.assertEquals("by_shirt", trackEventsSent.get(9).eventTypeId);
+        // Test post tracks
+        String postTracksData = new FileHelper().loadFileContent("tracks_1.json");
+        url = mWebServer.url("/tracks/");
+        HttpRequest postTracksReq = client.request(url.uri(), HttpClient.HTTP_POST, postTracksData);
+        HttpResponse postTracksResp = postTracksReq.execute();
+        RecordedRequest postTrackRecReq = mWebServer.takeRequest();
+        String receivedPostTrackData = postTrackRecReq.getBody().readUtf8();
+        List<Event> trackEventsSent = parseTrackEvents(receivedPostTrackData);
 
 
-            // Assert limit wrong request
-            Assert.assertEquals(300, limitNoSuccessResp.getHttpStatus());
-            Assert.assertFalse(limitNoSuccessResp.isSuccess());
-            Assert.assertNull(limitNoSuccessResp.getData());
+        // Test post impressions
+        String postImpData = new FileHelper().loadFileContent("impressions_1.json");
+        url = mWebServer.url("/impressions/");
+        HttpRequest postImpReq = client.request(url.uri(), HttpClient.HTTP_POST, postImpData);
+        HttpResponse postImpResp = postImpReq.execute();
+        RecordedRequest postImpRecReq = mWebServer.takeRequest();
+        String receivedPostImpData = postImpRecReq.getBody().readUtf8();
+        List<TestImpressions> impSent = parseImpressions(receivedPostImpData);
 
-            // Assert bad request
-            Assert.assertEquals(400, badResp.getHttpStatus());
-            Assert.assertFalse(badResp.isSuccess());
-            Assert.assertNull(badResp.getData());
-        }
+        // Test limit wrong request
+        url = mWebServer.url("/limit_wrong/");
+        HttpRequest limitNoSuccessReq = client.request(url.uri(), HttpClient.HTTP_GET);
+        HttpResponse limitNoSuccessResp = limitNoSuccessReq.execute();
+        recReq = mWebServer.takeRequest();
 
-        @After
-        public void tearDown() throws IOException {
-            mWebServer.shutdown();
-        }
+        // Test bad request
+        url = mWebServer.url("/bad/");
+        HttpRequest badReq = client.request(url.uri(), HttpClient.HTTP_GET);
+        HttpResponse badResp = badReq.execute();
+        recReq = mWebServer.takeRequest();
 
-        private void setupServer() throws IOException {
+        // Assert dummy request and response
+        Assert.assertEquals(200, dummyResp.getHttpStatus());
+        Assert.assertTrue(dummyResp.isSuccess());
+        Assert.assertEquals("this is split test", dummyResp.getData());
 
-            final String splitChangesResponse = new FileHelper().loadFileContent("split_changes_1.json");
+        // Assert my segments
+        List<MySegment> mySegments = parseMySegments(mySegmentsResp.getData());
+        Assert.assertEquals(200, mySegmentsResp.getHttpStatus());
+        Assert.assertEquals(2, mySegments.size());
+        Assert.assertEquals("id1", mySegments.get(0).id);
+        Assert.assertEquals("groupa", mySegments.get(0).name);
+        Assert.assertEquals("id2", mySegments.get(1).id);
+        Assert.assertEquals("groupb", mySegments.get(1).name);
 
-            mWebServer = new MockWebServer();
-            final Dispatcher dispatcher = new Dispatcher() {
+        // Assert split changes
+        SplitChange splitChange = Json.fromJson(splitChangeResp.getData(), SplitChange.class);
+        Assert.assertEquals(200, splitChangeResp.getHttpStatus());
+        Assert.assertTrue(splitChangeResp.isSuccess());
+        Assert.assertEquals(-1, splitChange.since);
+        Assert.assertEquals(1506703262916L, splitChange.till);
+        Assert.assertEquals(30, splitChange.splits.size());
 
-                @Override
-                public MockResponse dispatch (RecordedRequest request) throws InterruptedException {
+        // Assert empty response
+        Assert.assertEquals(200, emptyResp.getHttpStatus());
+        Assert.assertTrue(emptyResp.isSuccess());
+        Assert.assertNull(emptyResp.getData());
 
-                    switch (request.getPath()) {
-                        case "/test1/":
-                            return new MockResponse().setResponseCode(200).setBody("this is split test");
+        // Assert post non empty response
+        Assert.assertEquals(201, postDataResp.getHttpStatus());
+        Assert.assertTrue(postDataResp.isSuccess());
+        Assert.assertEquals("{\"resp\": 1 }", postDataResp.getData());
 
-                        case "/test2/":
+        // Assert empty response
+        Assert.assertEquals(201, postNoDataResp.getHttpStatus());
+        Assert.assertTrue(postNoDataResp.isSuccess());
+        Assert.assertNull(postNoDataResp.getData());
 
-                            return new MockResponse().setResponseCode(200).setBody("{\"mySegments\":[{\"id\":\"id1\", \"name\":\"groupa\"}, {\"id\":\"id2\", \"name\":\"groupb\"}]}");
-                        case "/test3/":
-                            return new MockResponse().setResponseCode(200).setBody(splitChangesResponse);
+        // Assert tracks
+        Assert.assertEquals(200, postTracksResp.getHttpStatus());
+        Assert.assertTrue(postTracksResp.isSuccess());
+        Assert.assertNull(postTracksResp.getData());
+        Assert.assertEquals(10, trackEventsSent.size());
+        Assert.assertEquals("open_web", trackEventsSent.get(0).eventTypeId);
+        Assert.assertEquals("CUSTOMER_ID", trackEventsSent.get(0).key);
+        Assert.assertEquals(1.0, trackEventsSent.get(0).value);
+        Assert.assertEquals("by_shirt", trackEventsSent.get(9).eventTypeId);
 
-                        case "/test4/":
-                            return new MockResponse().setResponseCode(200);
+        // Assert impressions
+        Assert.assertEquals(200, postImpResp.getHttpStatus());
+        Assert.assertTrue(postImpResp.isSuccess());
+        Assert.assertNull(postImpResp.getData());
+        Assert.assertEquals(2, impSent.size());
+        Assert.assertEquals("ANDROID_sameTreatmentWithBucketingKey", impSent.get(0).testName);
+        Assert.assertEquals(4, impSent.get(0).keyImpressions.size());
+        Assert.assertEquals("sample_feature", impSent.get(1).testName);
+        Assert.assertEquals(5, impSent.get(1).keyImpressions.size());
 
-                        case "/post_resp/":
-                            return new MockResponse().setResponseCode(201).setBody("{\"resp\": 1 }");
+        // Assert limit wrong request
+        Assert.assertEquals(300, limitNoSuccessResp.getHttpStatus());
+        Assert.assertFalse(limitNoSuccessResp.isSuccess());
+        Assert.assertNull(limitNoSuccessResp.getData());
 
-                        case "/post_no_resp/":
-                            return new MockResponse().setResponseCode(201);
+        // Assert bad request
+        Assert.assertEquals(400, badResp.getHttpStatus());
+        Assert.assertFalse(badResp.isSuccess());
+        Assert.assertNull(badResp.getData());
+    }
 
-                        case "/tracks/":
-                            return new MockResponse().setResponseCode(200);
+    @After
+    public void tearDown() throws IOException {
+        mWebServer.shutdown();
+    }
 
-                        case "/limit_wrong/":
-                            return new MockResponse().setResponseCode(300);
+    private void setupServer() throws IOException {
 
-                        case "/bad/":
-                            return new MockResponse().setResponseCode(400);
-                    }
-                    return new MockResponse().setResponseCode(404);
+        final String splitChangesResponse = new FileHelper().loadFileContent("split_changes_1.json");
+
+        mWebServer = new MockWebServer();
+        final Dispatcher dispatcher = new Dispatcher() {
+
+            @Override
+            public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
+
+                switch (request.getPath()) {
+                    case "/test1/":
+                        return new MockResponse().setResponseCode(200).setBody("this is split test");
+
+                    case "/test2/":
+
+                        return new MockResponse().setResponseCode(200).setBody("{\"mySegments\":[{\"id\":\"id1\", \"name\":\"groupa\"}, {\"id\":\"id2\", \"name\":\"groupb\"}]}");
+                    case "/test3/":
+                        return new MockResponse().setResponseCode(200).setBody(splitChangesResponse);
+
+                    case "/test4/":
+                        return new MockResponse().setResponseCode(200);
+
+                    case "/post_resp/":
+                        return new MockResponse().setResponseCode(201).setBody("{\"resp\": 1 }");
+
+                    case "/post_no_resp/":
+                        return new MockResponse().setResponseCode(201);
+
+                    case "/tracks/":
+                        return new MockResponse().setResponseCode(200);
+
+                    case "/impressions/":
+                        return new MockResponse().setResponseCode(200);
+
+                    case "/limit_wrong/":
+                        return new MockResponse().setResponseCode(300);
+
+                    case "/bad/":
+                        return new MockResponse().setResponseCode(400);
                 }
-            };
-            mWebServer.setDispatcher(dispatcher);
-            mWebServer.start();
-        }
+                return new MockResponse().setResponseCode(404);
+            }
+        };
+        mWebServer.setDispatcher(dispatcher);
+        mWebServer.start();
+    }
 
-        private void log(String m) {
-            System.out.println("FACTORY_TEST: " + m);
-        }
+    private void log(String m) {
+        System.out.println("FACTORY_TEST: " + m);
+    }
 
-        private List<MySegment> parseMySegments(String json) {
-            Type mapType = new TypeToken<Map<String, List<MySegment>>>() {
-            }.getType();
+    private List<MySegment> parseMySegments(String json) {
+        Type mapType = new TypeToken<Map<String, List<MySegment>>>() {
+        }.getType();
 
-            Map<String, List<MySegment>> mySegmentsMap = Json.fromJson(json, mapType);
-            return mySegmentsMap.get("mySegments");
-        }
+        Map<String, List<MySegment>> mySegmentsMap = Json.fromJson(json, mapType);
+        return mySegmentsMap.get("mySegments");
+    }
 
     private List<Event> parseTrackEvents(String json) {
         Type mapType = new TypeToken<List<Event>>() {
@@ -230,5 +253,13 @@ public class HttpClientTest {
         return Json.fromJson(json, mapType);
     }
 
+    private List<TestImpressions> parseImpressions(String json) {
+        Type mapType = new TypeToken<List<TestImpressions>>() {
+        }.getType();
+
+        return Json.fromJson(json, mapType);
     }
+
+
+}
 
