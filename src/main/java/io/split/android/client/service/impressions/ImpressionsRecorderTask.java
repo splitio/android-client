67e6f0f007e9b40d1524e7b44228d0e8f8290b5b
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-01-03 17:45:25 -0300

    Added impressions recorder task an tests

---
+++ src/main/java/io/split/android/client/service/impressions/ImpressionsRecorderTask.java
@@ -0,0 +1,58 @@
+package io.split.android.client.service.impressions;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+import io.split.android.client.dtos.KeyImpression;
+import io.split.android.client.service.executor.SplitTask;
+import io.split.android.client.service.executor.SplitTaskExecutionInfo;
+import io.split.android.client.service.executor.SplitTaskExecutionListener;
+import io.split.android.client.service.executor.SplitTaskExecutionStatus;
+import io.split.android.client.service.http.HttpRecorder;
+import io.split.android.client.service.http.HttpRecorderException;
+import io.split.android.client.storage.impressions.PersistentImpressionsStorage;
+import io.split.android.client.utils.Logger;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class ImpressionsRecorderTask implements SplitTask {
+
+    private final String mTaskId;
+    private final SplitTaskExecutionListener mExecutionListener;
+    private final PersistentImpressionsStorage mPersistenImpressionsStorage;
+    private final HttpRecorder<List<KeyImpression>> mHttpRecorder;
+    private final ImpressionsRecorderTaskConfig mConfig;
+
+    public ImpressionsRecorderTask(@NonNull String taskId,
+                                   @NonNull SplitTaskExecutionListener executionListener,
+                                   @NonNull HttpRecorder<List<KeyImpression>> httpRecorder,
+                                   @NonNull PersistentImpressionsStorage persistenEventsStorage,
+                                   @NonNull ImpressionsRecorderTaskConfig config) {
+        mTaskId = checkNotNull(taskId);
+        mExecutionListener = checkNotNull(executionListener);
+        mHttpRecorder = checkNotNull(httpRecorder);
+        mPersistenImpressionsStorage = checkNotNull(persistenEventsStorage);
+        mConfig = checkNotNull(config);
+    }
+
+    @Override
+    public void execute() {
+        SplitTaskExecutionStatus status = SplitTaskExecutionStatus.SUCCESS;
+        boolean sendMore = true;
+        while(sendMore) {
+            List<KeyImpression> events = mPersistenImpressionsStorage.pop(mConfig.getImpressionsPerPush());
+            if(events.size() > 0) {
+                try {
+                    mHttpRecorder.execute(events);
+                } catch (HttpRecorderException e) {
+                    status = SplitTaskExecutionStatus.ERROR;
+                    Logger.e("Event recorder task: Some events couldn't be sent");
+                }
+            }
+            sendMore = (events.size() == mConfig.getImpressionsPerPush());
+        }
+
+        mExecutionListener.taskExecuted(new SplitTaskExecutionInfo(mTaskId, status));
+    }
+}
