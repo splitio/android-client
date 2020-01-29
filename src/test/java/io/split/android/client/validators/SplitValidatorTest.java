742d18b5398e29c2c7f005c26f0b4f01a55ca917
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-01-29 16:12:14 -0300

    Remove unused code

--- src/test/java/io/split/android/client/validators/SplitValidatorTest.java
+++ src/test/java/io/split/android/client/validators/SplitValidatorTest.java
@@ -22,11 +22,6 @@ public class SplitValidatorTest {
 
     @Before
     public void setUp() {
-//        FileHelper fileHelper = new FileHelper();
-//        List<String> mySegments = Arrays.asList("s1", "s2", "test_copy");
-//        RefreshableMySegmentsFetcherProvider mySegmentsProvider = new RefreshableMySegmentsFetcherProviderStub(mySegments);
-//        List<Split> splits = fileHelper.loadAndParseSplitChangeFile("split_changes_1.json");
-//        SplitFetcher splitFetcher = new SplitFetcherStub(splits, mySegmentsProvider);
         validator = new SplitValidatorImpl();
     }
 
