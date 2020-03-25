509c0df3392b6e603cc53e537d5c2d54bb609d16
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-03-25 18:28:11 -0300

    Fixed exception test

--- src/test/java/io/split/android/client/service/SplitKillTaskTest.java
+++ src/test/java/io/split/android/client/service/SplitKillTaskTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import io.split.android.client.dtos.Split;
@@ -21,6 +22,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class SplitKillTaskTest {
 
@@ -32,18 +34,34 @@ public class SplitKillTaskTest {
     @Before
     public void setup() {
         mSplitsStorage = Mockito.mock(SplitsStorage.class);
+        Split split = new Split();
+        split.name = "split1";
+        split.defaultTreatment = "off";
+        split.changeNumber = 100;
+        split.killed = false;
         mTask = new SplitKillTask( mSplitsStorage);
+        when(mSplitsStorage.get(any())).thenReturn(split);
+
     }
 
     @Test
     public void correctExecution() throws HttpFetcherException {
         Split split = new Split();
+        split.name = "split1";
+        split.defaultTreatment = "on";
+        split.changeNumber = 1001;
         mTask.setParam(split);
 
         SplitTaskExecutionInfo result = mTask.execute();
-        verify(mSplitsStorage, times(1)).updateWithoutChecks(split);
+        ArgumentCaptor<Split> splitCaptor =
+                ArgumentCaptor.forClass(Split.class);
+        verify(mSplitsStorage, times(1)).updateWithoutChecks(splitCaptor.capture());
         Assert.assertEquals(SplitTaskExecutionStatus.SUCCESS, result.getStatus());
         Assert.assertEquals(SplitTaskType.SPLIT_KILL, result.getTaskType());
+        Assert.assertEquals(split.name, splitCaptor.getValue().name);
+        Assert.assertEquals(split.defaultTreatment, splitCaptor.getValue().defaultTreatment);
+        Assert.assertEquals(split.changeNumber, splitCaptor.getValue().changeNumber);
+        Assert.assertEquals(true, splitCaptor.getValue().killed);
     }
 
     @Test
@@ -55,11 +73,10 @@ public class SplitKillTaskTest {
     }
 
     @Test
-    public void storageException() throws HttpFetcherException {
+    public void storageException() {
         Split split = new Split();
         mTask.setParam(split);
-        doThrow(NullPointerException.class).when(mSplitsStorage).updateWithoutChecks(split);
-
+        doThrow(NullPointerException.class).when(mSplitsStorage).updateWithoutChecks(any());
         SplitTaskExecutionInfo result = mTask.execute();
         Assert.assertEquals(SplitTaskExecutionStatus.ERROR, result.getStatus());
     }
