6e23474d2741dd947f40daff5f701178e9aac2a0
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-05-27 12:13:17 -0300

    Added proxy settings model

--- src/main/java/io/split/android/client/network/HttpProxy.java
+++ src/main/java/io/split/android/client/network/HttpProxy.java
@@ -1,18 +1,38 @@
 package io.split.android.client.network;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import io.split.android.client.utils.Logger;
+
+import static androidx.core.util.Preconditions.checkNotNull;
+
 public class HttpProxy {
+
+    static final private String ADDRESS_SEPARATOR = ":";
     final private String address;
     final private int port;
     final private String username;
     final private String password;
 
-    public HttpProxy(String address, int port) {
-        this(address, port, null, null);
+    public HttpProxy(@NonNull String proxyAddress) {
+        this(proxyAddress, null, null);
     }
 
-    public HttpProxy(String address, int port, @Nullable String username, @Nullable String password) {
+    public HttpProxy(@NonNull String proxyAddress, @Nullable String username, @Nullable String password) {
+        checkNotNull(proxyAddress);
+
+        String address = proxyAddress;
+        int port = 0;
+        String[] proxyComponents = proxyAddress.split(ADDRESS_SEPARATOR);
+        if (proxyComponents.length > 1) {
+            address = proxyComponents[0];
+            try {
+                port = Integer.valueOf(proxyComponents[1]);
+            } catch (NumberFormatException e) {
+                Logger.e("Proxy port not valid. Using 0");
+            }
+        }
         this.address = address;
         this.port = port;
         this.username = username;
