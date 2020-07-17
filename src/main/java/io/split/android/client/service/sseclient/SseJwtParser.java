98e4737bda0b018dff9e397120815d9c1eaf8318
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-07-17 11:03:41 -0300

    Using dto to parse sse auth token

--- src/main/java/io/split/android/client/service/sseclient/SseJwtParser.java
+++ src/main/java/io/split/android/client/service/sseclient/SseJwtParser.java
@@ -18,9 +18,6 @@ import io.split.android.client.utils.Logger;
 
 public class SseJwtParser {
 
-    final static String CHANNEL_LIST_FIELD = "x-ably-capability";
-    final static String ISSUED_AT_FIELD = "iat";
-    final static String EXPIRATION_FIELD = "exp";
     private final static String PUBLISHERS_CHANNEL_METADATA = "channel-metadata:publishers";
     private final static String PUBLISHERS_CHANNEL_PREFIX = "[?occupancy=metrics.publishers]";
 
@@ -34,58 +31,56 @@ public class SseJwtParser {
 
         if (rawToken == null) {
             Logger.e("Error: JWT is null.");
-            throw  new InvalidJwtTokenException();
+            throw new InvalidJwtTokenException();
         }
 
         final String encodedPayload = extractTokenData(rawToken);
         if (encodedPayload == null) {
             Logger.e("SSE authentication JWT payload is not valid.");
-            throw  new InvalidJwtTokenException();
+            throw new InvalidJwtTokenException();
         }
 
         String payload = base64Decode(encodedPayload);
         if (payload == null) {
             Logger.e("Could not decode SSE authentication JWT payload.");
-            throw  new InvalidJwtTokenException();
+            throw new InvalidJwtTokenException();
         }
 
-        long issuedAtTime = 0;
-        long expirationTime = 0;
         Map<String, List<String>> channels = null;
+        SseAuthToken authToken = null;
         try {
-            Map<String, Object> allToken = Json.fromJson(payload, ALL_TOKEN_TYPE);
+            authToken = Json.fromJson(payload, SseAuthToken.class);
 
-            if (allToken == null) {
-                Logger.e("SSE JWT data is empty.");
-                throw  new InvalidJwtTokenException();
+            if (authToken == null || authToken.getChannelList() == null) {
+                Logger.e("SSE JWT data is empty or not valid.");
+                throw new InvalidJwtTokenException();
             }
-            String unparsedChannels = allToken.get(CHANNEL_LIST_FIELD).toString();
+            String unparsedChannels = authToken.getChannelList();
             channels = Json.fromJson(unparsedChannels, CHANNEL_TYPE);
             if (channels == null) {
                 Logger.e("SSE JWT has not channels.");
-                throw  new InvalidJwtTokenException();
+                throw new InvalidJwtTokenException();
             }
-            issuedAtTime = new Double(allToken.get(ISSUED_AT_FIELD).toString()).longValue();
-            expirationTime = new Double(allToken.get(EXPIRATION_FIELD).toString()).longValue();
+
         } catch (JsonSyntaxException e) {
             Logger.e("Error parsing SSE authentication JWT json " + e.getLocalizedMessage());
-            throw  new InvalidJwtTokenException();
+            throw new InvalidJwtTokenException();
         } catch (Exception e) {
             Logger.e("Unknonwn error while parsing SSE authentication JWT: " + e.getLocalizedMessage());
-            throw  new InvalidJwtTokenException();
+            throw new InvalidJwtTokenException();
         }
 
         List<String> processedChannels = new ArrayList<>();
-        for(String channel : channels.keySet()) {
+        for (String channel : channels.keySet()) {
             List<String> channelInfo = channels.get(channel);
-            if(channelInfo != null && channelInfo.contains(PUBLISHERS_CHANNEL_METADATA)) {
+            if (channelInfo != null && channelInfo.contains(PUBLISHERS_CHANNEL_METADATA)) {
                 processedChannels.add(PUBLISHERS_CHANNEL_PREFIX + channel);
             } else {
                 processedChannels.add(channel);
             }
         }
 
-        return new SseJwtToken(issuedAtTime, expirationTime, processedChannels, rawToken);
+        return new SseJwtToken(authToken.getIssuedAt(), authToken.getExpirationAt(), processedChannels, rawToken);
     }
 
     @Nullable
