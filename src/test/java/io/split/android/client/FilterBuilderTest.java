6663181ead74cbdbdc357ba7992c9bf3d9f7ecb0
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-07-31 16:44:17 -0300

    Merged with previous feature branch: Splits builder

--- src/test/java/io/split/android/client/FilterBuilderTest.java
+++ src/test/java/io/split/android/client/FilterBuilderTest.java
@@ -16,7 +16,7 @@ public class FilterBuilderTest {
         SplitFilter byNameFilter = SplitFilter.byName(Arrays.asList("nf_a", "nf_c", "nf_b"));
         SplitFilter byPrefixFilter = SplitFilter.byPrefix(Arrays.asList("pf_c", "pf_b", "pf_a"));
 
-        String queryString = new FilterBuilder().addFilter(byNameFilter).addFilter(byPrefixFilter).build();
+        String queryString = new FilterBuilder().addFilters(Arrays.asList(byNameFilter, byPrefixFilter)).build();
 
         Assert.assertEquals("&names=nf_a,nf_b,nf_c&prefixes=pf_a,pf_b,pf_c", queryString);
     }
@@ -28,8 +28,8 @@ public class FilterBuilderTest {
         SplitFilter byNameFilter = SplitFilter.byName(Arrays.asList("nf_a", "nf_c", "nf_b"));
         SplitFilter byPrefixFilter = SplitFilter.byPrefix(Arrays.asList("pf_c", "pf_b", "pf_a"));
 
-        String onlyByNameQs = new FilterBuilder().addFilter(byNameFilter).build();
-        String onlyByPrefixQs = new FilterBuilder().addFilter(byPrefixFilter).build();
+        String onlyByNameQs = new FilterBuilder().addFilters(Arrays.asList(byNameFilter)).build();
+        String onlyByPrefixQs = new FilterBuilder().addFilters(Arrays.asList(byPrefixFilter)).build();
 
         Assert.assertEquals("&names=nf_a,nf_b,nf_c", onlyByNameQs);
         Assert.assertEquals("&prefixes=pf_a,pf_b,pf_c", onlyByPrefixQs);
@@ -39,11 +39,14 @@ public class FilterBuilderTest {
     public void filterValuesDeduptedAndGrouped() {
         // Duplicated filter values should be removed on builing
 
+        List<SplitFilter> filters = Arrays.asList(
+                SplitFilter.byName(Arrays.asList("nf_a", "nf_c", "nf_b")),
+                SplitFilter.byName(Arrays.asList("nf_b", "nf_d")),
+                SplitFilter.byPrefix(Arrays.asList("pf_a", "pf_c", "pf_b")),
+                SplitFilter.byPrefix(Arrays.asList("pf_d", "pf_a")));
+
         String queryString = new FilterBuilder()
-                .addFilter(SplitFilter.byName(Arrays.asList("nf_a", "nf_c", "nf_b")))
-                .addFilter(SplitFilter.byName(Arrays.asList("nf_b", "nf_d")))
-                .addFilter(SplitFilter.byPrefix(Arrays.asList("pf_a", "pf_c", "pf_b")))
-                .addFilter(SplitFilter.byPrefix(Arrays.asList("pf_d", "pf_a")))
+                .addFilters(filters)
                 .build();
 
         Assert.assertEquals("&names=nf_a,nf_b,nf_c,nf_d&prefixes=pf_a,pf_b,pf_c,pf_d", queryString);
@@ -61,7 +64,7 @@ public class FilterBuilderTest {
 
         try {
             String queryString = new FilterBuilder()
-                    .addFilter(SplitFilter.byName(values))
+                    .addFilters(Arrays.asList(SplitFilter.byName(values)))
                     .build();
         } catch (Exception e) {
             exceptionThrown = true;
@@ -82,7 +85,7 @@ public class FilterBuilderTest {
 
         try {
             String queryString = new FilterBuilder()
-                    .addFilter(SplitFilter.byPrefix(values))
+                    .addFilters(Arrays.asList(SplitFilter.byPrefix(values)))
                     .build();
         } catch (Exception e) {
             exceptionThrown = true;
