2e345ee1995a75bc6f93adc8fd799b8449481e42
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-04-17 16:38:03 -0300

    Fixed issue when comparing segments

--- src/main/java/io/split/android/client/service/synchronizer/MySegmentsChangeChecker.java
+++ src/main/java/io/split/android/client/service/synchronizer/MySegmentsChangeChecker.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import io.split.android.client.utils.Logger;
+
 public class MySegmentsChangeChecker {
     public boolean mySegmentsHaveChanged(List<String> oldSegments, List<String> newSegments) {
         List<String> oldValues = new ArrayList<>(oldSegments);
         List<String> newValues = new ArrayList<>(newSegments);
         Collections.sort(oldValues);
         Collections.sort(newValues);
-        return !oldSegments.equals(newValues);
+        return !oldValues.equals(newValues);
     }
 }
