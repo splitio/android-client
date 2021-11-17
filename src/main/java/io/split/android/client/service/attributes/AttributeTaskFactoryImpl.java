8f9a653b305419778eea4e69f467849e29ff2279
Gaston Thea <gaston.thea@split.io>
2021-11-17 20:43:53 -0300

    Add missing implementation of Attribute task factory;

--- src/main/java/io/split/android/client/service/attributes/AttributeTaskFactoryImpl.java
+++ src/main/java/io/split/android/client/service/attributes/AttributeTaskFactoryImpl.java
@@ -8,11 +8,11 @@ public class AttributeTaskFactoryImpl implements AttributeTaskFactory {
 
     @Override
     public AttributeUpdateTask createAttributeUpdateTask(PersistentAttributesStorage persistentAttributesStorage, Map<String, Object> attributes) {
-        return null;
+        return new AttributeUpdateTask(persistentAttributesStorage, attributes);
     }
 
     @Override
     public AttributeClearTask createAttributeClearTask(PersistentAttributesStorage persistentAttributesStorage) {
-        return null;
+        return new AttributeClearTask(persistentAttributesStorage);
     }
 }
