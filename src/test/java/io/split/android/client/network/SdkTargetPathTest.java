130aac1ea0573e120511e1cd0ce43b30e57d9fb5
Gastón Thea <gaston.thea@split.io>
2021-11-24 10:37:56 -0300

    [SDKS-5325] Replace key encoding for segments path (#292)

---
+++ src/test/java/io/split/android/client/network/SdkTargetPathTest.java
@@ -0,0 +1,32 @@
+package io.split.android.client.network;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class SdkTargetPathTest {
+
+    @Test
+    public void userKeyWithSpaces() throws URISyntaxException {
+        URI uri = SdkTargetPath.mySegments("https://split.io", "CABM, CCIB Marketing");
+
+        assertEquals("/mySegments/CABM,%20CCIB%20Marketing", uri.getRawPath());
+    }
+
+    @Test
+    public void userKeyWithSlash() throws URISyntaxException {
+        URI uri = SdkTargetPath.mySegments("https://split.io", "user/key");
+
+        assertEquals("/mySegments/user%2Fkey", uri.getRawPath());
+    }
+
+    @Test
+    public void userKeyWithSpecialChars() throws URISyntaxException {
+        URI uri = SdkTargetPath.mySegments("https://split.io", "grüneStraße");
+
+        assertEquals("/mySegments/gr%C3%BCneStra%C3%9Fe", uri.getRawPath());
+    }
+}
