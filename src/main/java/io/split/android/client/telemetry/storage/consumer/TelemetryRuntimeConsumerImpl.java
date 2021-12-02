548836e6499850fb41f19190bb3707b3db0d1c6b
Gastón Thea <gaston.thea@split.io>
2021-12-02 16:57:51 -0300

    RuntimeConsumer

---
+++ src/main/java/io/split/android/client/telemetry/storage/consumer/TelemetryRuntimeConsumerImpl.java
@@ -0,0 +1,74 @@
+package io.split.android.client.telemetry.storage.consumer;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+import io.split.android.client.telemetry.model.EventsDataRecordsEnum;
+import io.split.android.client.telemetry.model.HTTPErrors;
+import io.split.android.client.telemetry.model.HTTPLatencies;
+import io.split.android.client.telemetry.model.ImpressionsDataType;
+import io.split.android.client.telemetry.model.LastSync;
+import io.split.android.client.telemetry.model.streaming.StreamingEvent;
+import io.split.android.client.telemetry.storage.TelemetryStorage;
+
+public class TelemetryRuntimeConsumerImpl implements TelemetryRuntimeConsumer {
+
+    private final TelemetryStorage mTelemetryStorage;
+
+    public TelemetryRuntimeConsumerImpl(@NonNull TelemetryStorage telemetryStorage) {
+        mTelemetryStorage = checkNotNull(telemetryStorage);
+    }
+
+    @Override
+    public long getImpressionsStats(ImpressionsDataType type) {
+        return mTelemetryStorage.getImpressionsStats(type);
+    }
+
+    @Override
+    public long getEventsStats(EventsDataRecordsEnum type) {
+        return mTelemetryStorage.getEventsStats(type);
+    }
+
+    @Override
+    public LastSync getLastSynchronization() {
+        return mTelemetryStorage.getLastSynchronization();
+    }
+
+    @Override
+    public HTTPErrors popHTTPErrors() {
+        return mTelemetryStorage.popHTTPErrors();
+    }
+
+    @Override
+    public HTTPLatencies popHttpLatencies() {
+        return mTelemetryStorage.popHttpLatencies();
+    }
+
+    @Override
+    public long popAuthRejections() {
+        return mTelemetryStorage.popAuthRejections();
+    }
+
+    @Override
+    public long popTokenRefreshes() {
+        return mTelemetryStorage.popTokenRefreshes();
+    }
+
+    @Override
+    public List<StreamingEvent> popStreamingEvents() {
+        return mTelemetryStorage.popStreamingEvents();
+    }
+
+    @Override
+    public List<String> popTags() {
+        return mTelemetryStorage.popTags();
+    }
+
+    @Override
+    public long getSessionLength() {
+        return mTelemetryStorage.getSessionLength();
+    }
+}
---
+++ src/test/java/io/split/android/client/telemetry/storage/consumer/TelemetryRuntimeConsumerImplTest.java
@@ -0,0 +1,161 @@
+package io.split.android.client.telemetry.storage.consumer;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.split.android.client.telemetry.model.EventsDataRecordsEnum;
+import io.split.android.client.telemetry.model.HTTPErrors;
+import io.split.android.client.telemetry.model.HTTPLatencies;
+import io.split.android.client.telemetry.model.ImpressionsDataType;
+import io.split.android.client.telemetry.model.LastSync;
+import io.split.android.client.telemetry.model.streaming.ConnectionEstablishedStreamingEvent;
+import io.split.android.client.telemetry.model.streaming.StreamingEvent;
+import io.split.android.client.telemetry.storage.TelemetryStorage;
+
+public class TelemetryRuntimeConsumerImplTest {
+
+    @Mock
+    private TelemetryStorage telemetryStorage;
+    private TelemetryRuntimeConsumer telemetryRuntimeConsumer;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        telemetryRuntimeConsumer = new TelemetryRuntimeConsumerImpl(telemetryStorage);
+    }
+
+    @Test
+    public void getImpressionsStatsFetchesValuesFromStorage() {
+        when(telemetryStorage.getImpressionsStats(ImpressionsDataType.IMPRESSIONS_QUEUED)).thenReturn(2L);
+        when(telemetryStorage.getImpressionsStats(ImpressionsDataType.IMPRESSIONS_DEDUPED)).thenReturn(1L);
+        when(telemetryStorage.getImpressionsStats(ImpressionsDataType.IMPRESSIONS_DROPPED)).thenReturn(3L);
+
+        long queuedImpressions = telemetryRuntimeConsumer.getImpressionsStats(ImpressionsDataType.IMPRESSIONS_QUEUED);
+        long dedupedImpressions = telemetryRuntimeConsumer.getImpressionsStats(ImpressionsDataType.IMPRESSIONS_DEDUPED);
+        long droppedImpressions = telemetryRuntimeConsumer.getImpressionsStats(ImpressionsDataType.IMPRESSIONS_DROPPED);
+
+        verify(telemetryStorage).getImpressionsStats(ImpressionsDataType.IMPRESSIONS_QUEUED);
+        verify(telemetryStorage).getImpressionsStats(ImpressionsDataType.IMPRESSIONS_DEDUPED);
+        verify(telemetryStorage).getImpressionsStats(ImpressionsDataType.IMPRESSIONS_DROPPED);
+        assertEquals(2L, queuedImpressions);
+        assertEquals(1L, dedupedImpressions);
+        assertEquals(3L, droppedImpressions);
+    }
+
+    @Test
+    public void getEventsStatsFetchesValuesFromStorage() {
+        when(telemetryStorage.getEventsStats(EventsDataRecordsEnum.EVENTS_DROPPED)).thenReturn(4L);
+        when(telemetryStorage.getEventsStats(EventsDataRecordsEnum.EVENTS_QUEUED)).thenReturn(2L);
+
+        long eventsDropped = telemetryRuntimeConsumer.getEventsStats(EventsDataRecordsEnum.EVENTS_DROPPED);
+        long eventsQueued = telemetryRuntimeConsumer.getEventsStats(EventsDataRecordsEnum.EVENTS_QUEUED);
+
+        verify(telemetryStorage).getEventsStats(EventsDataRecordsEnum.EVENTS_DROPPED);
+        verify(telemetryStorage).getEventsStats(EventsDataRecordsEnum.EVENTS_QUEUED);
+        assertEquals(4L, eventsDropped);
+        assertEquals(2L, eventsQueued);
+    }
+
+    @Test
+    public void getLastSynchronizationFetchesValuesFromStorage() {
+        LastSync mockLastSync = new LastSync();
+        mockLastSync.setLastTokenRefresh(1000);
+        when(telemetryStorage.getLastSynchronization()).thenReturn(mockLastSync);
+
+        LastSync lastSynchronization = telemetryRuntimeConsumer.getLastSynchronization();
+
+        verify(telemetryStorage).getLastSynchronization();
+        assertEquals(1000, lastSynchronization.getLastTokenRefresh());
+    }
+
+    @Test
+    public void popHttpErrorsFetchesValuesFromStorage() {
+        HTTPErrors mockHttpErrors = new HTTPErrors();
+        Map<Long, Long> errorMap = new HashMap<>();
+        errorMap.put(400L, 1L);
+        mockHttpErrors.setTokenGetErrs(errorMap);
+        when(telemetryStorage.popHTTPErrors()).thenReturn(mockHttpErrors);
+
+        HTTPErrors httpErrors = telemetryRuntimeConsumer.popHTTPErrors();
+
+        verify(telemetryStorage).popHTTPErrors();
+        assertEquals(errorMap, httpErrors.getTokenGetErrs());
+    }
+
+    @Test
+    public void popHttpLatenciesFetchesValuesFromStorage() {
+        HTTPLatencies mockHttpLatencies = new HTTPLatencies();
+        List<Long> mockBucketArray = Arrays.asList(1L, 4L);
+        mockHttpLatencies.setImpressionsCount(mockBucketArray);
+        when(telemetryStorage.popHttpLatencies()).thenReturn(mockHttpLatencies);
+
+        HTTPLatencies httpLatencies = telemetryRuntimeConsumer.popHttpLatencies();
+
+        verify(telemetryStorage).popHttpLatencies();
+        assertEquals(mockBucketArray, httpLatencies.getImpressionsCount());
+    }
+
+    @Test
+    public void popAuthRejectionsFetchesValuesFromStorage() {
+        when(telemetryStorage.popAuthRejections()).thenReturn(1L);
+
+        long authRejections = telemetryRuntimeConsumer.popAuthRejections();
+
+        verify(telemetryStorage).popAuthRejections();
+        assertEquals(1L, authRejections);
+    }
+
+    @Test
+    public void popTokenRefreshesFetchesValuesFromStorage() {
+        when(telemetryStorage.popTokenRefreshes()).thenReturn(4L);
+
+        long tokenRefreshes = telemetryRuntimeConsumer.popTokenRefreshes();
+
+        verify(telemetryStorage).popTokenRefreshes();
+        assertEquals(4L, tokenRefreshes);
+    }
+
+    @Test
+    public void popStreamingEventsFetchesValuesFromStorage() {
+        List<StreamingEvent> mockStreamingEventList = Collections.singletonList(new ConnectionEstablishedStreamingEvent(1000));
+        when(telemetryStorage.popStreamingEvents()).thenReturn(mockStreamingEventList);
+
+        List<StreamingEvent> streamingEvents = telemetryRuntimeConsumer.popStreamingEvents();
+
+        verify(telemetryStorage).popStreamingEvents();
+        assertEquals(mockStreamingEventList, streamingEvents);
+    }
+
+    @Test
+    public void popTagsFetchesValuesFromStorage() {
+        List<String> mockTagList = Arrays.asList("tag1", "tag2");
+        when(telemetryStorage.popTags()).thenReturn(mockTagList);
+
+        List<String> tags = telemetryRuntimeConsumer.popTags();
+
+        verify(telemetryStorage).popTags();
+        assertEquals(mockTagList, tags);
+    }
+
+    @Test
+    public void getSessionLengthFetchesValuesFromStorage() {
+        when(telemetryStorage.getSessionLength()).thenReturn(20L);
+
+        long sessionLength = telemetryRuntimeConsumer.getSessionLength();
+
+        verify(telemetryStorage).getSessionLength();
+        assertEquals(20L, sessionLength);
+    }
+}
