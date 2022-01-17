a07aed3fcc5c97e85476757b6541396c916ca33c
Gastón Thea <gaston.thea@split.io>
2022-01-17 18:18:08 -0300

    Telemetry in telemetry config task

--- src/test/java/io/split/android/client/service/telemetry/TelemetryConfigRecorderTaskTest.java
+++ src/test/java/io/split/android/client/service/telemetry/TelemetryConfigRecorderTaskTest.java
@@ -2,6 +2,8 @@ package io.split.android.client.service.telemetry;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -16,7 +18,9 @@ import java.util.Arrays;
 import io.split.android.client.service.http.HttpRecorder;
 import io.split.android.client.service.http.HttpRecorderException;
 import io.split.android.client.telemetry.model.Config;
+import io.split.android.client.telemetry.model.OperationType;
 import io.split.android.client.telemetry.storage.TelemetryConfigProvider;
+import io.split.android.client.telemetry.storage.TelemetryRuntimeProducer;
 
 public class TelemetryConfigRecorderTaskTest {
 
@@ -24,12 +28,14 @@ public class TelemetryConfigRecorderTaskTest {
     private HttpRecorder<Config> recorder;
     @Mock
     private TelemetryConfigProvider configProvider;
+    @Mock
+    private TelemetryRuntimeProducer telemetryRuntimeProducer;
     private TelemetryConfigRecorderTask telemetryConfigRecorderTask;
 
     @Before
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-        telemetryConfigRecorderTask = new TelemetryConfigRecorderTask(recorder, configProvider);
+        telemetryConfigRecorderTask = new TelemetryConfigRecorderTask(recorder, configProvider, telemetryRuntimeProducer);
     }
 
     @Test
@@ -62,5 +68,12 @@ public class TelemetryConfigRecorderTaskTest {
         verify(recorder).execute(argumentCaptor.capture());
         assertEquals(mockConfigTelemetry.isImpressionsListenerEnabled(), argumentCaptor.getValue().isImpressionsListenerEnabled());
         assertEquals(mockConfigTelemetry.getTags(), argumentCaptor.getValue().getTags());
+    }
+
+    @Test
+    public void latencyIsTrackedInTelemetry() {
+        telemetryConfigRecorderTask.execute();
+
+        verify(telemetryRuntimeProducer).recordSyncLatency(eq(OperationType.TELEMETRY), anyLong());
     }
 }
