ccb9ffd788bf591f0612bcd8fe10830860e92d64
Gastón Thea <gaston.thea@split.io>
2022-03-31 15:24:55 -0300

    Remove unused file

--- src/androidTest/java/tests/integration/shared/ServerMock.java
+++
@@ -1,9 +0,0 @@
-package tests.integration.shared;
-
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-
-import okhttp3.mockwebserver.Dispatcher;
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
