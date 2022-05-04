782a39b8151a923154c4ec28463d422c76e4510e
Gastón Thea <gaston.thea@split.io>
2022-05-04 16:23:12 -0300

    DTOs for MTKs (#373)

--- src/main/java/io/split/android/client/storage/impressions/PersistentImpressionsCountStorage.java
+++ src/main/java/io/split/android/client/storage/impressions/PersistentImpressionsCountStorage.java
@@ -1,18 +1,7 @@
 package io.split.android.client.storage.impressions;
 
-import androidx.annotation.NonNull;
-
-import java.util.List;
-
-import io.split.android.client.dtos.KeyImpression;
 import io.split.android.client.service.impressions.ImpressionsCountPerFeature;
-import io.split.android.client.storage.StoragePusher;
 
-public interface PersistentImpressionsCountStorage extends StoragePusher<ImpressionsCountPerFeature> {
-    // Push method is defined in StoragePusher interface
-    void pushMany(@NonNull List<ImpressionsCountPerFeature> counts);
-    List<ImpressionsCountPerFeature> pop(int count);
-    void setActive(@NonNull List<ImpressionsCountPerFeature> counts);
-    void delete(@NonNull List<ImpressionsCountPerFeature> counts);
-    void deleteInvalid(long maxTimestamp);
+public interface PersistentImpressionsCountStorage extends PersistentStorage<ImpressionsCountPerFeature> {
+
 }
