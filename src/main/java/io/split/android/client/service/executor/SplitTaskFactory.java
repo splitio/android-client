f6a183604fc9d6ed34586142cf95553e8196d347
Gastón Thea <gaston.thea@split.io>
2022-05-10 11:20:00 -0300

    Impression Manager (#381)

--- src/main/java/io/split/android/client/service/executor/SplitTaskFactory.java
+++ src/main/java/io/split/android/client/service/executor/SplitTaskFactory.java
@@ -1,14 +1,9 @@
 package io.split.android.client.service.executor;
 
-import java.util.List;
-
 import io.split.android.client.dtos.Split;
 import io.split.android.client.service.CleanUpDatabaseTask;
 import io.split.android.client.service.events.EventsRecorderTask;
-import io.split.android.client.service.impressions.ImpressionsCountPerFeature;
-import io.split.android.client.service.impressions.ImpressionsCountRecorderTask;
-import io.split.android.client.service.impressions.ImpressionsRecorderTask;
-import io.split.android.client.service.impressions.SaveImpressionsCountTask;
+import io.split.android.client.service.impressions.ImpressionsTaskFactory;
 import io.split.android.client.service.splits.FilterSplitsInCacheTask;
 import io.split.android.client.service.splits.LoadSplitsTask;
 import io.split.android.client.service.splits.SplitKillTask;
@@ -16,10 +11,9 @@ import io.split.android.client.service.splits.SplitsSyncTask;
 import io.split.android.client.service.splits.SplitsUpdateTask;
 import io.split.android.client.service.telemetry.TelemetryTaskFactory;
 
-public interface SplitTaskFactory extends TelemetryTaskFactory {
-    EventsRecorderTask createEventsRecorderTask();
+public interface SplitTaskFactory extends TelemetryTaskFactory, ImpressionsTaskFactory {
 
-    ImpressionsRecorderTask createImpressionsRecorderTask();
+    EventsRecorderTask createEventsRecorderTask();
 
     SplitsSyncTask createSplitsSyncTask(boolean checkCacheExpiration);
 
@@ -32,8 +26,4 @@ public interface SplitTaskFactory extends TelemetryTaskFactory {
     FilterSplitsInCacheTask createFilterSplitsInCacheTask();
 
     CleanUpDatabaseTask createCleanUpDatabaseTask(long maxTimestamp);
-
-    SaveImpressionsCountTask createSaveImpressionsCountTask(List<ImpressionsCountPerFeature> count);
-
-    ImpressionsCountRecorderTask createImpressionsCountRecorderTask();
 }
