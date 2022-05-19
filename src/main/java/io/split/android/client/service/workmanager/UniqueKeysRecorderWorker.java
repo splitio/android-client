5b3e4e695bd7d2b430b3f628efc871fa9509939a
Gastón Thea <gaston.thea@split.io>
2022-05-19 10:46:49 -0300

    Background job for unique keys (#385)

---
+++ src/main/java/io/split/android/client/service/workmanager/UniqueKeysRecorderWorker.java
@@ -0,0 +1,34 @@
+package io.split.android.client.service.workmanager;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Data;
+import androidx.work.WorkerParameters;
+
+import io.split.android.client.service.ServiceConstants;
+import io.split.android.client.service.ServiceFactory;
+import io.split.android.client.service.impressions.unique.UniqueKeysRecorderTask;
+import io.split.android.client.service.impressions.unique.UniqueKeysRecorderTaskConfig;
+import io.split.android.client.storage.db.StorageFactory;
+import io.split.android.client.utils.Logger;
+
+public class UniqueKeysRecorderWorker extends SplitWorker {
+
+    public UniqueKeysRecorderWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        try {
+            Data inputData = workerParams.getInputData();
+            mSplitTask = new UniqueKeysRecorderTask(ServiceFactory.getUniqueKeysRecorder(getNetworkHelper(),
+                    getHttpClient(),
+                    getEndPoint()),
+                    StorageFactory.getPersistentImpressionsUniqueStorage(getDatabase()),
+                    new UniqueKeysRecorderTaskConfig(
+                            inputData.getInt(ServiceConstants.WORKER_PARAM_UNIQUE_KEYS_PER_PUSH, ServiceConstants.DEFAULT_RECORDS_PER_PUSH),
+                            inputData.getLong(ServiceConstants.WORKER_PARAM_UNIQUE_KEYS_ESTIMATED_SIZE_IN_BYTES, ServiceConstants.ESTIMATED_IMPRESSION_SIZE_IN_BYTES)
+                    ));
+        } catch (Exception e) {
+            Logger.e("Error creating unique keys Split worker: " + e.getMessage());
+        }
+    }
+}
