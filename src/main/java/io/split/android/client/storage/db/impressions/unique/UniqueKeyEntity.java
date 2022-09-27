3dbd3272651db208d0da548a0d1f08fb8a6b5059
Gastón Thea <gaston.thea@split.io>
2022-09-27 15:24:48 -0300

    Fix UniqueKeyEntity setter

--- src/main/java/io/split/android/client/storage/db/impressions/unique/UniqueKeyEntity.java
+++ src/main/java/io/split/android/client/storage/db/impressions/unique/UniqueKeyEntity.java
@@ -78,7 +78,7 @@ public class UniqueKeyEntity implements Identifiable {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 }
