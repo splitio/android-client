0a250c094ec1fad6df6d000d9d972e37a1cc9fb1
Gastón Thea <gaston.thea@split.io>
2022-10-14 15:59:57 -0300

    Additional test for RetryBackoffCounterTimer

--- src/test/java/io/split/android/client/service/sseclient/sseclient/RetryBackoffCounterTimerTest.java
+++ src/test/java/io/split/android/client/service/sseclient/sseclient/RetryBackoffCounterTimerTest.java
@@ -1,16 +1,23 @@
 package io.split.android.client.service.sseclient.sseclient;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import androidx.annotation.NonNull;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import io.split.android.client.service.executor.SplitTask;
 import io.split.android.client.service.executor.SplitTaskExecutionInfo;
+import io.split.android.client.service.executor.SplitTaskExecutionListener;
 import io.split.android.client.service.executor.SplitTaskExecutor;
 import io.split.android.client.service.executor.SplitTaskType;
 import io.split.android.client.service.sseclient.BackoffCounter;
@@ -77,5 +84,35 @@ public class RetryBackoffCounterTimerTest {
         counterTimer.start();
 
         verify(taskExecutor, times(3)).schedule(mockTask, 0L, counterTimer);
+    }
+
+    @Test
+    public void testWithListener() throws InterruptedException {
+        final CountDownLatch listenerLatch = new CountDownLatch(1);
+        counterTimer = new RetryBackoffCounterTimer(taskExecutor, backoffCounter, 2);
+        when(taskExecutor.schedule(mockTask,
+                0L,
+                counterTimer))
+                .then(invocation -> {
+                    counterTimer.taskExecuted(SplitTaskExecutionInfo.error(SplitTaskType.GENERIC_TASK));
+                    return "100";
+                })
+                .then(invocation -> {
+                    counterTimer.taskExecuted(SplitTaskExecutionInfo.success(SplitTaskType.GENERIC_TASK));
+                    return "100";
+                });
+
+        counterTimer.setTask(mockTask, new SplitTaskExecutionListener() {
+            @Override
+            public void taskExecuted(@NonNull SplitTaskExecutionInfo taskInfo) {
+                listenerLatch.countDown();
+            }
+        });
+
+        counterTimer.start();
+        boolean await = listenerLatch.await(10, TimeUnit.SECONDS);
+
+        assertTrue(await);
+        verify(taskExecutor, times(2)).schedule(mockTask, 0L, counterTimer);
     }
 }
