9941562584e013aaf73a2ce0cb65e228d31d1312
Gastón Thea <gaston.thea@split.io>
2023-01-17 17:40:23 -0300

    Debug strategy

---
+++ src/main/java/io/split/android/client/service/impressions/strategy/DebugStrategy.java
@@ -0,0 +1,50 @@
+package io.split.android.client.service.impressions.strategy;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import androidx.annotation.NonNull;
+
+import io.split.android.client.dtos.KeyImpression;
+import io.split.android.client.impressions.Impression;
+import io.split.android.client.service.executor.SplitTaskExecutor;
+import io.split.android.client.service.impressions.ImpressionsObserver;
+import io.split.android.client.service.impressions.ImpressionsTaskFactory;
+import io.split.android.client.service.synchronizer.RecorderSyncHelper;
+import io.split.android.client.telemetry.model.ImpressionsDataType;
+import io.split.android.client.telemetry.storage.TelemetryRuntimeProducer;
+
+class DebugStrategy implements ProcessStrategy {
+
+    private final ImpressionsObserver mImpressionsObserver;
+    private final RecorderSyncHelper<KeyImpression> mImpressionsSyncHelper;
+    private final SplitTaskExecutor mTaskExecutor;
+    private final TelemetryRuntimeProducer mTelemetryRuntimeProducer;
+    private final ImpressionsTaskFactory mImpressionsTaskFactory;
+
+    public DebugStrategy(@NonNull ImpressionsObserver impressionsObserver,
+                         @NonNull RecorderSyncHelper<KeyImpression> impressionsSyncHelper,
+                         @NonNull SplitTaskExecutor taskExecutor,
+                         @NonNull ImpressionsTaskFactory taskFactory,
+                         @NonNull TelemetryRuntimeProducer telemetryRuntimeProducer) {
+        mImpressionsObserver = checkNotNull(impressionsObserver);
+        mImpressionsSyncHelper = checkNotNull(impressionsSyncHelper);
+        mTaskExecutor = checkNotNull(taskExecutor);
+        mImpressionsTaskFactory = checkNotNull(taskFactory);
+        mTelemetryRuntimeProducer = checkNotNull(telemetryRuntimeProducer);
+    }
+
+    @Override
+    public void apply(@NonNull Impression impression) {
+        Long previousTime = mImpressionsObserver.testAndSet(impression);
+        impression = impression.withPreviousTime(previousTime);
+
+        KeyImpression keyImpression = KeyImpression.fromImpression(impression);
+        if (mImpressionsSyncHelper.pushAndCheckIfFlushNeeded(keyImpression)) {
+            mTaskExecutor.submit(
+                    mImpressionsTaskFactory.createImpressionsRecorderTask(),
+                    mImpressionsSyncHelper);
+        }
+
+        mTelemetryRuntimeProducer.recordImpressionStats(ImpressionsDataType.IMPRESSIONS_QUEUED, 1);
+    }
+}
---
+++ src/test/java/io/split/android/client/service/impressions/strategy/DebugStrategyTest.kt
@@ -0,0 +1,77 @@
+package io.split.android.client.service.impressions.strategy
+
+import io.split.android.client.dtos.KeyImpression
+import io.split.android.client.service.executor.SplitTaskExecutor
+import io.split.android.client.service.impressions.ImpressionsObserver
+import io.split.android.client.service.impressions.ImpressionsRecorderTask
+import io.split.android.client.service.impressions.ImpressionsTaskFactory
+import io.split.android.client.service.synchronizer.RecorderSyncHelper
+import io.split.android.client.telemetry.model.ImpressionsDataType
+import io.split.android.client.telemetry.storage.TelemetryRuntimeProducer
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+
+class DebugStrategyTest {
+
+    @Mock
+    private lateinit var taskExecutor: SplitTaskExecutor
+
+    @Mock
+    private lateinit var taskFactory: ImpressionsTaskFactory
+
+    @Mock
+    private lateinit var impressionsObserver: ImpressionsObserver
+
+    @Mock
+    private lateinit var impressionsSyncHelper: RecorderSyncHelper<KeyImpression>
+
+    @Mock
+    private lateinit var telemetryRuntimeProducer: TelemetryRuntimeProducer
+
+    private lateinit var strategy: DebugStrategy
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        strategy = DebugStrategy(
+            impressionsObserver,
+            impressionsSyncHelper,
+            taskExecutor,
+            taskFactory,
+            telemetryRuntimeProducer
+        )
+    }
+
+    @Test
+    fun `recorded impression is tracked in telemetry`() {
+        strategy.apply(createUniqueImpression())
+
+        verify(telemetryRuntimeProducer).recordImpressionStats(ImpressionsDataType.IMPRESSIONS_QUEUED, 1)
+    }
+
+    @Test
+    fun `impression are flushed when flush is needed`() {
+        val impressionsRecorderTask = mock(ImpressionsRecorderTask::class.java)
+        `when`(taskFactory.createImpressionsRecorderTask()).thenReturn(impressionsRecorderTask)
+        `when`(impressionsSyncHelper.pushAndCheckIfFlushNeeded(any())).thenReturn(true)
+
+        strategy.apply(createUniqueImpression())
+
+        verify(taskExecutor).submit(impressionsRecorderTask, impressionsSyncHelper)
+    }
+
+    @Test
+    fun `impression are not flushed when flush is not needed`() {
+        val impressionsRecorderTask = mock(ImpressionsRecorderTask::class.java)
+        `when`(taskFactory.createImpressionsRecorderTask()).thenReturn(impressionsRecorderTask)
+        `when`(impressionsSyncHelper.pushAndCheckIfFlushNeeded(any())).thenReturn(false)
+
+        strategy.apply(createUniqueImpression())
+
+        verify(taskExecutor, never()).submit(impressionsRecorderTask, impressionsSyncHelper)
+    }
+}
