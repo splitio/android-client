e5e985d9fc96270291bf116d9b97cfbf0ef6f8e9
Gastón Thea <gaston.thea@split.io>
2023-06-13 16:28:29 -0300

    Sync guardian (#507)

---
+++ src/main/java/io/split/android/client/service/synchronizer/SyncGuardianImpl.java
@@ -0,0 +1,45 @@
+package io.split.android.client.service.synchronizer;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import io.split.android.client.SplitClientConfig;
+
+public class SyncGuardianImpl implements SyncGuardian {
+
+    private final AtomicLong mDefaultMaxSyncPeriod;
+    private final AtomicLong mMaxSyncPeriod;
+    private final AtomicLong mLastSyncTimestamp;
+    private final TimestampProvider mNewTimestamp;
+    private final boolean mSyncEnabled;
+    private final boolean mStreamingEnabled;
+
+    SyncGuardianImpl(long maxSyncPeriod, SplitClientConfig splitConfig, TimestampProvider timestampProvider) {
+        mDefaultMaxSyncPeriod = new AtomicLong(maxSyncPeriod);
+        mMaxSyncPeriod = new AtomicLong(maxSyncPeriod);
+        mLastSyncTimestamp = new AtomicLong(0);
+        mSyncEnabled = splitConfig.syncEnabled();
+        mStreamingEnabled = splitConfig.streamingEnabled();
+        mNewTimestamp = timestampProvider != null ? timestampProvider : System::currentTimeMillis;
+    }
+
+    @Override
+    public void updateLastSyncTimestamp() {
+        mLastSyncTimestamp.set(mNewTimestamp.get());
+    }
+
+    @Override
+    public boolean mustSync() {
+        return mSyncEnabled && mStreamingEnabled &&
+                mNewTimestamp.get() - mLastSyncTimestamp.get() >= mMaxSyncPeriod.get();
+    }
+
+    @Override
+    public void setMaxSyncPeriod(long newPeriod) {
+        mMaxSyncPeriod.set(Math.max(newPeriod, mDefaultMaxSyncPeriod.get()));
+    }
+
+    interface TimestampProvider {
+
+        long get();
+    }
+}
