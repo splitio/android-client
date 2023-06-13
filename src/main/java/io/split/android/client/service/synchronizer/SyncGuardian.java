9b46f191d09378407b049b7e428c3771c0b75db6
Gaston Thea <gaston.thea@split.io>
2023-06-13 14:59:56 -0300

    SyncGuardian

---
+++ src/main/java/io/split/android/client/service/synchronizer/SyncGuardian.java
@@ -0,0 +1,10 @@
+package io.split.android.client.service.synchronizer;
+
+public interface SyncGuardian {
+
+    void setMaxSyncPeriod(long maxSyncPeriod);
+
+    void updateLastSyncTimestamp();
+
+    boolean mustSync();
+}
