ee4574f7484d56dc3bceea76089ce638597c47cb
Gaston Thea <gaston.thea@split.io>
2023-06-22 12:46:20 -0300

    Improved test

--- src/test/java/io/split/android/client/service/sseclient/sseclient/notifications/SplitsChangeNotificationTest.java
+++ src/test/java/io/split/android/client/service/sseclient/sseclient/notifications/SplitsChangeNotificationTest.java
@@ -11,7 +11,9 @@ import io.split.android.client.utils.Json;
 
 public class SplitsChangeNotificationTest {
 
-    private static final String FULL_NOTIFICATION = "{\"type\":\"SPLIT_UPDATE\",\"changeNumber\":1684265694505,\"pcn\":0,\"c\":2,\"d\":\"redacted=\"}";
+    private static final String FULL_NOTIFICATION_C0 = "{\"type\":\"SPLIT_UPDATE\",\"changeNumber\":1684265694505,\"pcn\":0,\"c\":0,\"d\":\"redacted=\"}";
+    private static final String FULL_NOTIFICATION_C1 = "{\"type\":\"SPLIT_UPDATE\",\"changeNumber\":1684265694505,\"pcn\":0,\"c\":1,\"d\":\"redacted=\"}";
+    private static final String FULL_NOTIFICATION_C2 = "{\"type\":\"SPLIT_UPDATE\",\"changeNumber\":1684265694505,\"pcn\":0,\"c\":2,\"d\":\"redacted=\"}";
 
     private static final String LEGACY_NOTIFICATION = "{\"type\":\"SPLIT_UPDATE\",\"changeNumber\":1684265694505}";
 
@@ -27,11 +29,15 @@ public class SplitsChangeNotificationTest {
 
     @Test
     public void valuesAreCorrectlyDeserialized() {
-        SplitsChangeNotification splitsChangeNotification = Json.fromJson(FULL_NOTIFICATION, SplitsChangeNotification.class);
+        SplitsChangeNotification c0Notification = Json.fromJson(FULL_NOTIFICATION_C0, SplitsChangeNotification.class);
+        SplitsChangeNotification c1Notification = Json.fromJson(FULL_NOTIFICATION_C1, SplitsChangeNotification.class);
+        SplitsChangeNotification c2Notification = Json.fromJson(FULL_NOTIFICATION_C2, SplitsChangeNotification.class);
 
-        assertEquals(1684265694505L, splitsChangeNotification.getChangeNumber());
-        assertEquals(0L, splitsChangeNotification.getPreviousChangeNumber().longValue());
-        assertEquals("redacted=", splitsChangeNotification.getData());
-        assertEquals(CompressionType.ZLIB, splitsChangeNotification.getCompressionType());
+        assertEquals(CompressionType.NONE, c0Notification.getCompressionType());
+        assertEquals(CompressionType.GZIP, c1Notification.getCompressionType());
+        assertEquals(1684265694505L, c2Notification.getChangeNumber());
+        assertEquals(0L, c2Notification.getPreviousChangeNumber().longValue());
+        assertEquals("redacted=", c2Notification.getData());
+        assertEquals(CompressionType.ZLIB, c2Notification.getCompressionType());
     }
 }
