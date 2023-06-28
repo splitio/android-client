a75afff58e0a9b7341665b9faad84621f5525c47
Gaston Thea <gaston.thea@split.io>
2023-06-28 15:00:15 -0300

    New change notification integration tests

--- src/main/java/io/split/android/client/service/sseclient/notifications/RawNotification.java
+++ src/main/java/io/split/android/client/service/sseclient/notifications/RawNotification.java
@@ -1,11 +1,18 @@
 package io.split.android.client.service.sseclient.notifications;
 
+import com.google.gson.annotations.SerializedName;
+
 public class RawNotification {
 
+    @SerializedName("clientId")
     private String clientId;
+    @SerializedName("name")
     private String name;
+    @SerializedName("data")
     private String data;
+    @SerializedName("channel")
     private String channel;
+    @SerializedName("timestamp")
     private long timestamp;
 
     public String getClientId() {
