052f77453563cbd8b397405fb72ac4eee624166c
Gaston Thea <gaston.thea@split.io>
2023-08-30 17:50:15 -0300

    Fix name

--- src/main/java/io/split/android/client/service/executor/SplitTaskFactory.java
+++ src/main/java/io/split/android/client/service/executor/SplitTaskFactory.java
@@ -18,7 +18,7 @@ public interface SplitTaskFactory extends TelemetryTaskFactory, ImpressionsTaskF
 
     SplitsSyncTask createSplitsSyncTask(boolean checkCacheExpiration);
 
-    LoadSplitsTask createLoadSplitsTask(String createLoadSplitsTask);
+    LoadSplitsTask createLoadSplitsTask(String splitsFilterQueryStringFromConfig);
 
     SplitKillTask createSplitKillTask(Split split);
 
