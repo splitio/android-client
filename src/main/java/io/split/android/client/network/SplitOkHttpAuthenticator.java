488dba3cd414a65336242042675f22545352bad9
Gastón Thea <gaston.thea@split.io>
2023-11-29 14:01:06 -0300

    New authenticator (#568)

---
+++ src/main/java/io/split/android/client/network/SplitOkHttpAuthenticator.java
@@ -0,0 +1,49 @@
+package io.split.android.client.network;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+import java.util.Map;
+
+import io.split.android.client.utils.logger.Logger;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.Route;
+
+class SplitOkHttpAuthenticator implements okhttp3.Authenticator {
+
+    private final SplitAuthenticator mSplitAuthenticator;
+
+    SplitOkHttpAuthenticator(SplitAuthenticator splitAuthenticator) {
+        mSplitAuthenticator = splitAuthenticator;
+    }
+
+    @Nullable
+    @Override
+    public Request authenticate(@Nullable Route route, @NonNull Response response) {
+
+        try {
+            SplitAuthenticatedRequest authenticatedRequestResult = mSplitAuthenticator.authenticate(new SplitAuthenticatedRequest(response));
+            if (authenticatedRequestResult == null) {
+                return null;
+            }
+
+            Request.Builder builder = response.request()
+                    .newBuilder();
+
+            if (authenticatedRequestResult.getHeaders() != null) {
+                for (Map.Entry<String, List<String>> header : authenticatedRequestResult.getHeaders().entrySet()) {
+                    for (String value : header.getValue()) {
+                        builder.addHeader(header.getKey(), value);
+                    }
+                }
+            }
+
+            return builder.build();
+        } catch (Exception exception) {
+            Logger.e("Error authenticating request: ", exception.getMessage());
+            return null;
+        }
+    }
+}
