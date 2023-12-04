11b8c2ea54855f79955b8d441a91f511406a266c
Gaston Thea <gaston.thea@split.io>
2023-12-04 14:11:50 -0300

    Internal authenticator tests

--- src/test/java/io/split/android/client/network/SplitUrlConnectionAuthenticatorTest.java
+++ src/test/java/io/split/android/client/network/SplitUrlConnectionAuthenticatorTest.java
@@ -1,8 +1,18 @@
 package io.split.android.client.network;
 
+import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.net.HttpURLConnection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SplitUrlConnectionAuthenticatorTest {
 
@@ -15,5 +25,73 @@ public class SplitUrlConnectionAuthenticatorTest {
         mAuthenticator = new SplitUrlConnectionAuthenticator(mSplitAuthenticator);
     }
 
-    //TODO
+    @Test
+    public void originalConnectionIsReturnedWhenAuthenticationResultIsNull() {
+        when(mSplitAuthenticator.authenticate(any())).thenReturn(null);
+
+        HttpURLConnection mockConnection = mock(HttpURLConnection.class);
+        HttpURLConnection authenticate = mAuthenticator.authenticate(mockConnection);
+
+        assertSame(authenticate, mockConnection);
+    }
+
+    @Test
+    public void headersFromAuthenticatedRequestAreAddedToConnection() {
+        SplitAuthenticatedRequest authRequest = mock(SplitAuthenticatedRequest.class);
+        Map<String, String> authHeaders = new HashMap<>();
+        authHeaders.put("header", "value");
+        authHeaders.put("header2", "value2");
+        when(authRequest.getHeaders()).thenReturn(authHeaders);
+        when(mSplitAuthenticator.authenticate(any())).thenReturn(authRequest);
+
+        HttpURLConnection mockConnection = mock(HttpURLConnection.class);
+        mAuthenticator.authenticate(mockConnection);
+
+        verify(mockConnection).addRequestProperty("header", "value");
+        verify(mockConnection).addRequestProperty("header2", "value2");
+    }
+
+    @Test
+    public void nullHeadersAreIgnored() {
+        SplitAuthenticatedRequest authRequest = mock(SplitAuthenticatedRequest.class);
+        Map<String, String> authHeaders = new HashMap<>();
+        authHeaders.put("header", "value");
+        authHeaders.put("header2", null);
+        when(authRequest.getHeaders()).thenReturn(authHeaders);
+        when(mSplitAuthenticator.authenticate(any())).thenReturn(authRequest);
+
+        HttpURLConnection mockConnection = mock(HttpURLConnection.class);
+        mAuthenticator.authenticate(mockConnection);
+
+        verify(mockConnection).addRequestProperty("header", "value");
+        verify(mockConnection, times(0)).addRequestProperty("header2", null);
+    }
+
+    @Test
+    public void nullKeysInHeadersAreIgnored() {
+        SplitAuthenticatedRequest authRequest = mock(SplitAuthenticatedRequest.class);
+        Map<String, String> authHeaders = new HashMap<>();
+        authHeaders.put("header", "value");
+        authHeaders.put(null, "value2");
+        when(authRequest.getHeaders()).thenReturn(authHeaders);
+        when(mSplitAuthenticator.authenticate(any())).thenReturn(authRequest);
+
+        HttpURLConnection mockConnection = mock(HttpURLConnection.class);
+        mAuthenticator.authenticate(mockConnection);
+
+        verify(mockConnection).addRequestProperty("header", "value");
+        verify(mockConnection, times(0)).addRequestProperty(null, "value2");
+    }
+
+    @Test
+    public void noHeadersAreAddedWhenHeadersFromAuthRequestIsNull() {
+        SplitAuthenticatedRequest authRequest = mock(SplitAuthenticatedRequest.class);
+        when(authRequest.getHeaders()).thenReturn(null);
+        when(mSplitAuthenticator.authenticate(any())).thenReturn(authRequest);
+
+        HttpURLConnection mockConnection = mock(HttpURLConnection.class);
+        mAuthenticator.authenticate(mockConnection);
+
+        verify(mockConnection, times(0)).addRequestProperty(any(), any());
+    }
 }
