5461f25a7240655a7942143725b507bf76184cac
Gaston Thea <gaston.thea@split.io>
2023-12-07 12:59:43 -0300

    New authenticator

--- src/test/java/io/split/android/client/network/SplitAuthenticatorTest.java
+++ src/test/java/io/split/android/client/network/SplitAuthenticatorTest.java
@@ -28,24 +28,24 @@ public class SplitAuthenticatorTest {
         };
 
         AuthenticatedMockRequest request = new AuthenticatedMockRequest(new MockRequest());
-        Map<String, List<String>> initialHeaders = new HashMap<>(request.getHeaders());
+        Map<String, String> initialHeaders = new HashMap<>(request.getHeaders());
 
         splitAuthenticator.authenticate(request);
 
-        Map<String, List<String>> finalHeaders = new HashMap<>(request.getHeaders());
+        Map<String, String> finalHeaders = new HashMap<>(request.getHeaders());
 
         assertEquals(2, initialHeaders.size());
         assertTrue(initialHeaders.containsKey("header1"));
         assertTrue(initialHeaders.containsKey("header2"));
-        assertEquals(Collections.singletonList("value1"), initialHeaders.get("header1"));
-        assertEquals(Collections.singletonList("value2"), initialHeaders.get("header2"));
+        assertEquals("value1", initialHeaders.get("header1"));
+        assertEquals("value2", initialHeaders.get("header2"));
         assertEquals(3, finalHeaders.size());
         assertTrue(finalHeaders.containsKey("header1"));
         assertTrue(finalHeaders.containsKey("header2"));
         assertTrue(finalHeaders.containsKey("new-header"));
-        assertEquals(Collections.singletonList("value1"), finalHeaders.get("header1"));
-        assertEquals(Collections.singletonList("value2"), finalHeaders.get("header2"));
-        assertEquals(Collections.singletonList("value"), finalHeaders.get("new-header"));
+        assertEquals("value1", finalHeaders.get("header1"));
+        assertEquals("value2", finalHeaders.get("header2"));
+        assertEquals("value", finalHeaders.get("new-header"));
     }
 
     private static class AuthenticatedMockRequest implements AuthenticatedRequest<MockRequest> {
@@ -68,13 +68,8 @@ public class SplitAuthenticatorTest {
 
         @Nullable
         @Override
-        public Map<String, List<String>> getHeaders() {
+        public Map<String, String> getHeaders() {
             return mRequest.getHeaders();
-        }
-
-        @Override
-        public int getStatusCode() {
-            return 0;
         }
 
         @Override
@@ -84,30 +79,22 @@ public class SplitAuthenticatorTest {
     }
 
     private static class MockRequest {
-        private final Map<String, List<String>> mHeaders = new ConcurrentHashMap<>();
+        private final Map<String, String> mHeaders = new ConcurrentHashMap<>();
 
         MockRequest() {
-            mHeaders.put("header1", Collections.singletonList("value1"));
-            mHeaders.put("header2", Collections.singletonList("value2"));
+            mHeaders.put("header1", "value1");
+            mHeaders.put("header2", "value2");
         }
 
         public void setHeader(@NonNull String name, @NonNull String value) {
-            if (mHeaders.get(name) != null) {
-                mHeaders.get(name).add(value);
-            } else {
-                mHeaders.put(name, Collections.singletonList(value));
-            }
+            mHeaders.put(name, value);
         }
 
         public String getHeader(@NonNull String name) {
-            if (mHeaders.get(name) != null && mHeaders.get(name).size() > 0) {
-                return mHeaders.get(name).get(0);
-            }
-
-            return null;
+            return mHeaders.get(name);
         }
 
-        public Map<String, List<String>> getHeaders() {
+        public Map<String, String> getHeaders() {
             return mHeaders;
         }
     }
