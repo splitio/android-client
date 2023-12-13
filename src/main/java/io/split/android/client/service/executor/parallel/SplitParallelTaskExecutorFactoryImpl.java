9871e8cf1a10d087dd77ca2019fc16fc2d18b512
Gaston Thea <gaston.thea@split.io>
2023-12-13 18:31:55 -0300

    Add thread factory to parallel task executor scheduler

--- src/main/java/io/split/android/client/service/executor/parallel/SplitParallelTaskExecutorFactoryImpl.java
+++ src/main/java/io/split/android/client/service/executor/parallel/SplitParallelTaskExecutorFactoryImpl.java
@@ -1,14 +1,27 @@
 package io.split.android.client.service.executor.parallel;
 
+import androidx.annotation.NonNull;
+
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import io.split.android.client.service.executor.ThreadFactoryBuilder;
+import io.split.android.client.utils.logger.Logger;
 
 public class SplitParallelTaskExecutorFactoryImpl implements SplitParallelTaskExecutorFactory {
 
     private final int mThreads = Runtime.getRuntime().availableProcessors();
 
-    private final ExecutorService mScheduler = Executors.newFixedThreadPool(mThreads);
+    private final ExecutorService mScheduler = Executors.newFixedThreadPool(mThreads,
+            new ThreadFactoryBuilder()
+                    .setNameFormat("Split-ParallelTaskExecutor-%d")
+                    .setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+                        @Override
+                        public void uncaughtException(@NonNull Thread t, @NonNull Throwable e) {
+                            Logger.e("Unexpected error " + e.getLocalizedMessage());
+                        }
+                    }).build());
 
     @Override
     public <T> SplitParallelTaskExecutor<List<T>> createForList(Class<T> type) {
