c7876d473195fd388a6acc99f2d9a21466eae351
Gaston Thea <gaston.thea@split.io>
2024-03-08 15:26:02 -0300

    Javadoc

--- src/main/java/io/split/android/client/service/impressions/observer/ListenableLruCache.java
+++ src/main/java/io/split/android/client/service/impressions/observer/ListenableLruCache.java
@@ -4,6 +4,15 @@ import android.util.LruCache;
 
 import androidx.annotation.Nullable;
 
+/**
+ * Extension of {@link LruCache} that allows to listen to cache evictions.
+ * <p>
+ * The listener is notified when an entry is evicted from the cache by informing the key of the
+ * evicted entry.
+ *
+ * @param <K> Key type
+ * @param <V> Value type
+ */
 class ListenableLruCache<K, V> extends LruCache<K, V> {
 
     private final RemovalListener<K> mRemovalListener;
@@ -21,8 +30,18 @@ class ListenableLruCache<K, V> extends LruCache<K, V> {
         }
     }
 
+    /**
+     * Listener to be notified when an entry is evicted from the cache.
+     *
+     * @param <T> Key type
+     */
     interface RemovalListener<T> {
 
+        /**
+         * Called when an entry is evicted from the cache.
+         *
+         * @param key The key of the evicted entry
+         */
         void onRemoval(T key);
     }
 }
