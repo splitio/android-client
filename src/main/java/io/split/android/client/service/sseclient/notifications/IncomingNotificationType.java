5ccfbab8de4ac97e85c46899f9412e786a9238fa
Gaston Thea <gaston.thea@harness.io>
2024-08-29 16:42:20 -0300

    Restore type param

--- src/main/java/io/split/android/client/service/sseclient/notifications/IncomingNotificationType.java
+++ src/main/java/io/split/android/client/service/sseclient/notifications/IncomingNotificationType.java
@@ -3,7 +3,7 @@ package io.split.android.client.service.sseclient.notifications;
 import com.google.gson.annotations.SerializedName;
 
 public class IncomingNotificationType {
-    @SerializedName(value = "type", alternate = {"t"})
+    @SerializedName(value = "type")
     protected NotificationType type;
 
     public NotificationType getType() {
