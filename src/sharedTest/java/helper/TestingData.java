3e94a68912b19f1a195c1c63f5c8aa95f26fd565
Gaston Thea <gaston.thea@harness.io>
2024-09-03 14:55:37 -0300

    Rename remaining mocks

--- src/sharedTest/java/helper/TestingData.java
+++ src/sharedTest/java/helper/TestingData.java
@@ -6,7 +6,7 @@ import io.split.android.client.utils.Json;
 public class TestingData {
 
     public final static String UNBOUNDED_NOTIFICATION = "{" +
-            "\\\"type\\\": \\\"MEMBERSHIP_MS_UPDATE\\\"," +
+            "\\\"type\\\": \\\"MEMBERSHIPS_MS_UPDATE\\\"," +
             "\\\"u\\\": 0," +
             "\\\"c\\\": 0," +
             "\\\"d\\\": \\\"\\\"," +
@@ -15,7 +15,7 @@ public class TestingData {
             "}";
 
     public final static String SEGMENT_REMOVAL_NOTIFICATION = "{" +
-            "\\\"type\\\": \\\"MEMBERSHIP_MS_UPDATE\\\"," +
+            "\\\"type\\\": \\\"MEMBERSHIPS_MS_UPDATE\\\"," +
             "\\\"u\\\": 3," +
             "\\\"c\\\": 0," +
             "\\\"d\\\": \\\"\\\"," +
@@ -39,28 +39,28 @@ public class TestingData {
       8492584437244343049 11382796718859679607 11383137936375052427 17699699514337596928 17001541343685934583 8355202062888946034]
      */
     public final static String BOUNDED_NOTIFICATION_GZIP = "{" +
-            "\"type\": \"MEMBERSHIP_MS_UPDATE\"," +
+            "\"type\": \"MEMBERSHIPS_MS_UPDATE\"," +
             "\"u\": 1," +
             "\"c\": 1," +
             "\"d\": \"H4sIAAAAAAAA/2IYBfgAx0A7YBTgB4wD7YABAAID7QC6g5EYy8MEMA20A+gMFAbaAYMZDPXqlGWgHTAKRsEoGAWjgCzQQFjJkKqiiPAPAQAIAAD//5L7VQwAEAAA\"" +
             "}";
 
     public final static String ESCAPED_BOUNDED_NOTIFICATION_GZIP = "{" +
-            "\\\"type\\\": \\\"MEMBERSHIP_MS_UPDATE\\\"," +
+            "\\\"type\\\": \\\"MEMBERSHIPS_MS_UPDATE\\\"," +
             "\\\"u\\\": 1," +
             "\\\"c\\\": 1," +
             "\\\"d\\\": \\\"H4sIAAAAAAAA/2IYBfgAx0A7YBTgB4wD7YABAAID7QC6g5EYy8MEMA20A+gMFAbaAYMZDPXqlGWgHTAKRsEoGAWjgCzQQFjJkKqiiPAPAQAIAAD//5L7VQwAEAAA\\\"" +
             "}";
 
     public final static String BOUNDED_NOTIFICATION_ZLIB = "{" +
-            "\"type\": \"MEMBERSHIP_MS_UPDATE\"," +
+            "\"type\": \"MEMBERSHIPS_MS_UPDATE\"," +
             "\"u\": 1," +
             "\"c\": 2," +
             "\"d\": \"eJxiGAX4AMdAO2AU4AeMA+2AAQACA+0AuoORGMvDBDANtAPoDBQG2gGDGQz16pRloB0wCkbBKBgFo4As0EBYyZCqoojwDwEACAAA//+W/QFR\"" +
             "}";
 
     public final static String ESCAPED_BOUNDED_NOTIFICATION_ZLIB = "{" +
-            "\\\"type\\\": \\\"MEMBERSHIP_MS_UPDATE\\\"," +
+            "\\\"type\\\": \\\"MEMBERSHIPS_MS_UPDATE\\\"," +
             "\\\"u\\\": 1," +
             "\\\"c\\\": 2," +
             "\\\"d\\\": \\\"eJxiGAX4AMdAO2AU4AeMA+2AAQACA+0AuoORGMvDBDANtAPoDBQG2gGDGQz16pRloB0wCkbBKBgFo4As0EBYyZCqoojwDwEACAAA//+W/QFR\\\"" +
@@ -68,7 +68,7 @@ public class TestingData {
 
 
     public final static String ESCAPED_BOUNDED_NOTIFICATION_MALFORMED = "{" +
-            "\\\"type\\\": \\\"MEMBERSHIP_MS_UPDATE\\\"," +
+            "\\\"type\\\": \\\"MEMBERSHIPS_MS_UPDATE\\\"," +
             "\\\"u\\\": 1," +
             "\\\"c\\\": 1," +
             "\\\"d\\\": \\\"H4sIAAAAAAAAg5EYy8MEMA20A+//5L7VQwAEAAA\\\"" +
@@ -80,14 +80,14 @@ public class TestingData {
      * = a: [key1, key2] , r: [key3, key4]
      */
     public final static String KEY_LIST_NOTIFICATION_GZIP = "{" +
-            "\"type\": \"MEMBERSHIP_MS_UPDATE\"," +
+            "\"type\": \"MEMBERSHIPS_MS_UPDATE\"," +
             "\"u\": 2," +
             "\"c\": 1," +
             "\"d\": \"H4sIAAAAAAAA/wTAsRHDUAgD0F2ofwEIkPAqPhdZIW0uu/v97GPXHU004ULuMGrYR6XUbIjlXULPPse+dt1yhJibBODjrTmj3GJ4emduuDDP/w0AAP//18WLsl0AAAA=\"" +
             "}";
 
     public final static String ESCAPED_KEY_LIST_NOTIFICATION_GZIP = "{" +
-            "\\\"type\\\": \\\"MEMBERSHIP_MS_UPDATE\\\"," +
+            "\\\"type\\\": \\\"MEMBERSHIPS_MS_UPDATE\\\"," +
             "\\\"n\\\": [\\\"new_segment_added\\\"]," +
             "\\\"u\\\": 2," +
             "\\\"c\\\": 1," +
@@ -96,7 +96,7 @@ public class TestingData {
 
 public final static String BOUNDED_NOTIFICATION_ZLIB_2 = "{" +
             "\"cn\": 1629754722111, " +
-            "\"type\": \"MEMBERSHIP_MS_UPDATE\"," +
+            "\"type\": \"MEMBERSHIPS_MS_UPDATE\"," +
             "\"u\": 1," +
             "\"c\": 2," +
             "\"d\": \"eJzMVk3OhDAIVdNFl9/22zVzEo8yR5mjT6LGsRTKg2LiW8yPUnjQB+2kIwM2ThTIKtVU1oknFcRzufz+YGYM/phnHW8sdPvs9EzXW2I+HFzhNyTNgCD/PpW9xpGiHD0Bw1U5HLSS644FbGZgoPovmjpmX5wAzhIxJyN7IAnFQWX1htj+LUl6ZQRV3umMqYG1LCrOJGLPV8+IidBQZFt6sOUA6CqsX5iEFY2gqufs2mfqRtsVWytRnO+iYMN7xIBqJhDqAydV+HidkGOGEJYvk4fhe/8iIukphG/XfFcfVxnMVcALCOF77qL/EU7ODepxlLST6qxFLYRdOyW8EBY4BqVjObnm3V5ZMkZIKf++8+hM7zM1Kd3aFqVZeSHzDQAA//+QUQ3a\"" +
@@ -105,7 +105,7 @@ public final static String BOUNDED_NOTIFICATION_ZLIB_2 = "{" +
 //    cn: 1629754722111
 //    d: "eJzMVk3OhDAIVdNFl9/22zVzEo8yR5mjT6LGsRTKg2LiW8yPUnjQB+2kIwM2ThTIKtVU1oknFcRzufz+YGYM/phnHW8sdPvs9EzXW2I+HFzhNyTNgCD/PpW9xpGiHD0Bw1U5HLSS644FbGZgoPovmjpmX5wAzhIxJyN7IAnFQWX1htj+LUl6ZQRV3umMqYG1LCrOJGLPV8+IidBQZFt6sOUA6CqsX5iEFY2gqufs2mfqRtsVWytRnO+iYMN7xIBqJhDqAydV+HidkGOGEJYvk4fhe/8iIukphG/XfFcfVxnMVcALCOF77qL/EU7ODepxlLST6qxFLYRdOyW8EBY4BqVjObnm3V5ZMkZIKf++8+hM7zM1Kd3aFqVZeSHzDQAA//+QUQ3a"
 //    n: ""
-//    type: "MEMBERSHIP_MS_UPDATE"
+//    type: "MEMBERSHIPS_MS_UPDATE"
 //    u: 1
 
     public final static String DECOMPRESSED_KEY_LIST_PAYLOAD_GZIP = "{\"a\":[1573573083296714675,8482869187405483569],\"r\":[8031872927333060586,6829471020522910836]}";
