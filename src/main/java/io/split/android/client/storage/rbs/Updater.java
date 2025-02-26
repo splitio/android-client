73d30448ea442fd6e2fec793c1b1f2c6b8864fee
gthea <gaston.thea@harness.io>
2025-02-26 15:07:51 -0300

    SqLite persistent storage implementation for RBS (#741)

---
+++ src/main/java/io/split/android/client/storage/rbs/Updater.java
@@ -0,0 +1,84 @@
+package io.split.android.client.storage.rbs;
+
+import static io.split.android.client.utils.Utils.checkNotNull;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import io.split.android.client.dtos.RuleBasedSegment;
+import io.split.android.client.storage.cipher.SplitCipher;
+import io.split.android.client.storage.db.rbs.RuleBasedSegmentDao;
+import io.split.android.client.storage.db.rbs.RuleBasedSegmentEntity;
+import io.split.android.client.storage.general.GeneralInfoStorage;
+import io.split.android.client.utils.Json;
+import io.split.android.client.utils.logger.Logger;
+
+final class Updater implements Runnable {
+
+    @NonNull
+    private final SplitCipher mCipher;
+    @NonNull
+    private final GeneralInfoStorage mGeneralInfoStorage;
+    @NonNull
+    private final RuleBasedSegmentDao mDao;
+    @NonNull
+    private final Set<RuleBasedSegment> mToAdd;
+    @NonNull
+    private final Set<RuleBasedSegment> mToRemove;
+    private final long mChangeNumber;
+
+    Updater(@NonNull SplitCipher cipher,
+            @NonNull RuleBasedSegmentDao dao,
+            @NonNull GeneralInfoStorage generalInfoStorage,
+            @NonNull Set<RuleBasedSegment> toAdd,
+            @NonNull Set<RuleBasedSegment> toRemove,
+            long changeNumber) {
+        mCipher = checkNotNull(cipher);
+        mDao = checkNotNull(dao);
+        mGeneralInfoStorage = checkNotNull(generalInfoStorage);
+        mToAdd = checkNotNull(toAdd);
+        mToRemove = checkNotNull(toRemove);
+        mChangeNumber = changeNumber;
+    }
+
+    @Override
+    public void run() {
+        try {
+            List<String> toDelete = new ArrayList<>();
+            for (RuleBasedSegment segment : mToRemove) {
+                String encryptedName = mCipher.encrypt(segment.getName());
+                if (encryptedName != null) {
+                    toDelete.add(encryptedName);
+                }
+            }
+
+            List<RuleBasedSegmentEntity> toAdd = new ArrayList<>();
+            for (RuleBasedSegment segment : mToAdd) {
+                if (segment == null) {
+                    continue;
+                }
+
+                try {
+                    String name = mCipher.encrypt(segment.getName());
+                    String body = mCipher.encrypt(Json.toJson(segment));
+                    if (name == null || body == null) {
+                        continue;
+                    }
+                    toAdd.add(new RuleBasedSegmentEntity(name, body, System.currentTimeMillis()));
+                } catch (Exception e) {
+                    Logger.e("Error parsing RBS with name " + segment.getName() + ": " + e.getLocalizedMessage());
+                }
+            }
+
+            mDao.delete(toDelete);
+            mDao.insert(toAdd);
+            mGeneralInfoStorage.setRbsChangeNumber(mChangeNumber);
+        } catch (Exception e) {
+            Logger.e("Error updating RBS: " + e.getLocalizedMessage());
+            throw e;
+        }
+    }
+}
