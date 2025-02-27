e6ad3388b63be4beed7bf946113e040412213024
gthea <gaston.thea@harness.io>
2025-02-27 18:30:51 -0300

    RBS Storage provider (#742)

---
+++ src/main/java/io/split/android/client/localhost/LocalhostRuleBasedSegmentsStorageProvider.java
@@ -0,0 +1,20 @@
+package io.split.android.client.localhost;
+
+import androidx.annotation.NonNull;
+
+import io.split.android.client.storage.rbs.RuleBasedSegmentStorage;
+import io.split.android.client.storage.rbs.RuleBasedSegmentStorageProvider;
+
+class LocalhostRuleBasedSegmentsStorageProvider implements RuleBasedSegmentStorageProvider {
+    
+    private final RuleBasedSegmentStorage mRuleBasedSegmentStorage;
+    
+    LocalhostRuleBasedSegmentsStorageProvider(@NonNull RuleBasedSegmentStorage ruleBasedSegmentStorage) {
+        mRuleBasedSegmentStorage = ruleBasedSegmentStorage;
+    }
+
+    @Override
+    public RuleBasedSegmentStorage get() {
+        return mRuleBasedSegmentStorage;
+    }
+}
