deb1c7ce78214007104bfa9f3f25a120b0ca963b
Gaston Thea <gaston.thea@harness.io>
2025-04-25 16:56:28 -0300

    Update version

--- src/main/java/io/split/android/client/service/impressions/observer/ImpressionsObserverImpl.java
+++ src/main/java/io/split/android/client/service/impressions/observer/ImpressionsObserverImpl.java
@@ -27,10 +27,6 @@ public class ImpressionsObserverImpl implements ImpressionsObserver {
         if (null == impression) {
             return null;
         }
-        final String properties = impression.properties();
-        if (properties != null && !properties.isEmpty()) {
-            return null;
-        }
 
         Long hash = ImpressionHasher.process(impression);
         @Nullable
