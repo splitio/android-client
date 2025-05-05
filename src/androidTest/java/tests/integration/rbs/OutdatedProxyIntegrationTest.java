3f04af43dbacc6fd6c029e574ef71d24f115d2f2
Gaston Thea <gaston.thea@harness.io>
2025-05-05 14:39:48 -0300

    Integration tests for outdated proxy handling

--- src/androidTest/java/tests/integration/rbs/OutdatedProxyIntegrationTest.java
+++ src/androidTest/java/tests/integration/rbs/OutdatedProxyIntegrationTest.java
@@ -1,6 +1,8 @@
 package tests.integration.rbs;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static helper.IntegrationHelper.emptyTargetingRulesChanges;
 import static helper.IntegrationHelper.getSinceFromUri;
@@ -8,8 +10,10 @@ import static helper.IntegrationHelper.getSpecFromUri;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,8 +24,8 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
+import helper.DatabaseHelper;
 import helper.IntegrationHelper;
 import helper.TestableSplitConfigBuilder;
 import io.split.android.client.ServiceEndpoints;
@@ -29,8 +33,8 @@ import io.split.android.client.SplitClient;
 import io.split.android.client.SplitFactory;
 import io.split.android.client.TestingConfig;
 import io.split.android.client.events.SplitEvent;
+import io.split.android.client.storage.db.GeneralInfoEntity;
 import io.split.android.client.storage.db.SplitRoomDatabase;
-import io.split.android.client.utils.logger.Logger;
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
@@ -44,32 +48,26 @@ public class OutdatedProxyIntegrationTest {
     private MockWebServer mWebServer;
     private Map<String, AtomicInteger> mEndpointHits;
     private Map<String, CountDownLatch> mLatches;
-    private final AtomicLong mMySegmentsDelay = new AtomicLong(0L);
-    private final AtomicBoolean mRandomizeMyLargeSegments = new AtomicBoolean(false);
-    private final AtomicBoolean mEmptyMyLargeSegments = new AtomicBoolean(false);
     private final AtomicBoolean mOutdatedProxy = new AtomicBoolean(false);
+    private final AtomicBoolean mSimulatedProxyError = new AtomicBoolean(false);
+    private final AtomicBoolean mRecoveryHit = new AtomicBoolean(false);
 
     @Before
     public void setUp() throws IOException {
         mEndpointHits = new ConcurrentHashMap<>();
-        mMySegmentsDelay.set(0L);
-        mRandomizeMyLargeSegments.set(false);
-        mEmptyMyLargeSegments.set(false);
         mOutdatedProxy.set(false);
         initializeLatches();
 
         mWebServer = new MockWebServer();
         mWebServer.setDispatcher(new Dispatcher() {
+            @NonNull
             @Override
-            public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
-                System.out.println("Receiving request to " + request.getRequestUrl().toString());
-
+            public MockResponse dispatch(@NonNull RecordedRequest request) {
                 if (request.getRequestUrl().encodedPathSegments().contains(IntegrationHelper.ServicePath.SPLIT_CHANGES)) {
                     updateEndpointHit(IntegrationHelper.ServicePath.SPLIT_CHANGES);
-                    Logger.i("TEST: uri is " + request.getRequestUrl().uri());
                     float specFromUri = Float.parseFloat(getSpecFromUri(request.getRequestUrl().uri()));
                     if (mOutdatedProxy.get() && specFromUri > 1.2f) {
-                        Logger.i("TEST: Spec from uri is greater than 1.2");
+                        mSimulatedProxyError.set(true);
                         return new MockResponse().setResponseCode(400);
                     } else if (mOutdatedProxy.get()) {
                             String body = (getSinceFromUri(request.getRequestUrl().uri()).equals("-1")) ?
@@ -78,9 +76,15 @@ public class OutdatedProxyIntegrationTest {
                         return new MockResponse().setResponseCode(200)
                                 .setBody(body);
                     }
+
+                    if (!mOutdatedProxy.get()) {
+                        if (request.getRequestUrl().uri().toString().contains("?s=1.3&since=-1&rbSince=-1")) {
+                            mRecoveryHit.set(true);
+                        }
+                    }
+
                     return new MockResponse().setResponseCode(200).setBody(IntegrationHelper.loadSplitChanges(mContext, "split_changes_rbs.json"));
                 } else if (request.getRequestUrl().encodedPathSegments().contains(IntegrationHelper.ServicePath.MEMBERSHIPS)) {
-                    Thread.sleep(mMySegmentsDelay.get());
                     updateEndpointHit(IntegrationHelper.ServicePath.MEMBERSHIPS);
 
                     return new MockResponse().setResponseCode(200).setBody(IntegrationHelper.emptyAllSegments());
@@ -92,12 +96,41 @@ public class OutdatedProxyIntegrationTest {
         mWebServer.start();
     }
 
+    @After
+    public void tearDown() throws IOException {
+        mWebServer.shutdown();
+    }
+
     @Test
-    public void test() {
+    public void clientIsReadyEvenWhenUsingOutdatedProxy() {
         mOutdatedProxy.set(true);
         SplitClient readyClient = getReadyClient(IntegrationHelper.dummyUserKey().matchingKey(), getFactory());
 
         assertNotNull(readyClient);
+        assertFalse(mRecoveryHit.get());
+        assertTrue(mSimulatedProxyError.get());
+    }
+
+    @Test
+    public void clientIsReadyWithLatestProxy() {
+        mOutdatedProxy.set(false);
+        SplitClient readyClient = getReadyClient(IntegrationHelper.dummyUserKey().matchingKey(), getFactory());
+
+        assertNotNull(readyClient);
+        assertFalse(mRecoveryHit.get() && mOutdatedProxy.get());
+        assertFalse(mSimulatedProxyError.get());
+    }
+
+    @Test
+    public void clientRecoversFromOutdatedProxy() {
+        mOutdatedProxy.set(false);
+        SplitRoomDatabase database = DatabaseHelper.getTestDatabase(mContext);
+        database.generalInfoDao().update(new GeneralInfoEntity("lastProxyCheckTimestamp", System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(62)));
+        SplitClient readyClient = getReadyClient(IntegrationHelper.dummyUserKey().matchingKey(), getFactory(database));
+
+        assertNotNull(readyClient);
+        assertTrue(mRecoveryHit.get() && !mOutdatedProxy.get());
+        assertFalse(mSimulatedProxyError.get());
     }
 
     private void initializeLatches() {
@@ -119,10 +152,10 @@ public class OutdatedProxyIntegrationTest {
     }
 
     protected SplitFactory getFactory() {
-        return getFactory(10000, null);
+        return getFactory(null);
     }
 
-    protected SplitFactory getFactory(Integer ready, SplitRoomDatabase database) {
+    protected SplitFactory getFactory(SplitRoomDatabase database) {
         TestableSplitConfigBuilder configBuilder = new TestableSplitConfigBuilder()
                 .enableDebug()
                 .serviceEndpoints(ServiceEndpoints.builder()
@@ -130,9 +163,7 @@ public class OutdatedProxyIntegrationTest {
                         .build());
 
             configBuilder.streamingEnabled(false);
-        if (ready != null) {
-            configBuilder.ready(ready);
-        }
+        configBuilder.ready(10000);
         TestingConfig testingConfig = new TestingConfig();
         testingConfig.setFlagsSpec("1.3");
         return IntegrationHelper.buildFactory(
