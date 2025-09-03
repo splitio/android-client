8e4b72a864613298421834aee1c5429ad5974831
Gaston Thea <gaston.thea@harness.io>
2025-09-03 16:57:02 -0300

    Additional test deps

--- src/test/java/android/util/Base64.java
+++ src/test/java/android/util/Base64.java
@@ -10,4 +10,12 @@ public class Base64 {
     public static String encodeToString(byte[] input, int flags) {
         return java.util.Base64.getEncoder().encodeToString(input);
     }
+
+    public static byte[] decode(String input, int flags) {
+        return java.util.Base64.getDecoder().decode(input);
+    }
+
+    public static byte[] encode(byte[] input, int flags) {
+        return java.util.Base64.getEncoder().encode(input);
+    }
 }
