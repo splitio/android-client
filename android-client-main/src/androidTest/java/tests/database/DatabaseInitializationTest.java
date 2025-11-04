2de606604d9dfc2e5630c24af789fe9c41e88292
Gaston Thea <gaston.thea@harness.io>
2025-11-04 15:58:13 -0300

    Remove unnecessary code

--- android-client-main/src/androidTest/java/tests/database/DatabaseInitializationTest.java
+++ android-client-main/src/androidTest/java/tests/database/DatabaseInitializationTest.java
@@ -36,11 +36,6 @@ public class DatabaseInitializationTest {
     @Before
     public void setUp() throws IOException {
         mContext = InstrumentationRegistry.getInstrumentation().getContext();
-        // clear package data
-        String[] dbList = getDbList(mContext);
-        for (String db : dbList) {
-            DatabaseHelper.removeDatabaseFile(db);
-        }
         mHttpClientMock = new HttpClientMock(new HttpResponseMockDispatcher() {
             @Override
             public HttpResponseMock getResponse(URI uri, HttpMethod method, String body) {
@@ -195,10 +190,6 @@ public class DatabaseInitializationTest {
             throw new RuntimeException(e);
         }
         return Arrays.stream(context.databaseList())
-//                .map(db -> {
-//                    System.out.println("DB name: " + db);
-//                    return db;
-//                })
                 .filter(db -> !db.endsWith("-journal") && !db.endsWith("-wal") && !db.endsWith("-shm"))
                 .toArray(String[]::new);
     }
