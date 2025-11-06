015ef86006d16a60cb1a503939bf37903778bf9b
Gaston Thea <gaston.thea@harness.io>
2025-11-06 11:39:07 -0300

    Change version; expose SplitLogLevel

---
+++ android-client-main/src/main/java/io/split/android/client/utils/logger/SplitLogLevel.java
@@ -0,0 +1,40 @@
+package io.split.android.client.utils.logger;
+
+public class SplitLogLevel {
+
+    /**
+     * Priority constant for the println method; use Logger.v.
+     */
+    public static final int VERBOSE = 2;
+
+    /**
+     * Priority constant for the println method; use Logger.d.
+     */
+    public static final int DEBUG = 3;
+
+    /**
+     * Priority constant for the println method; use Logger.i.
+     */
+    public static final int INFO = 4;
+
+    /**
+     * Priority constant for the println method; use Loggger.w.
+     */
+    public static final int WARNING = 5;
+
+    /**
+     * Priority constant for the println method; use Logger.e.
+     */
+    public static final int ERROR = 6;
+
+    /**
+     * Priority constant for the println method; use Logger.wtf.
+     */
+    public static final int ASSERT = 7;
+
+    /**
+     * Turns off logging
+     */
+    public static final int NONE = -100;
+
+}
