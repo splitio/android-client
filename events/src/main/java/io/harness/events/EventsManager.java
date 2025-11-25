c95de14a69031150eeda069bfaca440e619ee449
Gaston Thea <gaston.thea@harness.io>
2025-11-25 10:46:40 -0300

    Add unregister method

--- events/src/main/java/io/harness/events/EventsManager.java
+++ events/src/main/java/io/harness/events/EventsManager.java
@@ -12,12 +12,19 @@ import androidx.annotation.Nullable;
 public interface EventsManager<E, I, M> {
 
     /**
-     * Registers a callback to be executed when the event is triggered.
+     * Registers a handler to be executed when the event is triggered.
      *
      * @param event   event to register
-     * @param handler callback to execute when the event is triggered
+     * @param handler handler to execute when the event is triggered
      */
     void register(E event, EventHandler<E, M> handler);
+
+    /**
+     * Unregisters all registered handlers for an event.
+     *
+     * @param event event to unregister handlers for
+     */
+    void unregister(E event);
 
     /**
      * Notifies an internal event has occurred.
